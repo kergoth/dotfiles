7a06ff8d412c6176ecb52d9456d6532d4c7fdd8d
Chris Larson <clarson@kergoth.com>
2006-04-20 00:59:21 +0000

    Clean up the LUA_INIT file.

--- .lua/init.lua
+++ .lua/init.lua
@@ -1,30 +1,27 @@
---if maj and tonumber(maj) >= 5 then
---	if min and tonumber(min) >= 1 then
---		local mt = getmetatable("")
---
---		function mt:__index(ind)
---			return string.sub(self, ind, ind)
---		end
---	end
---end
-
-
-local _,_,maj,min = string.find(_VERSION, "%s(%d)%p(%d)")
-if maj and min then
-	if tonumber(maj) >= 5 then
-		min = tonumber(min)
-
-		local home = os.getenv("HOME")
-		if min < 1 then
-			--require("compat-5.1")
-			LUA_PATH = "./?.lua;./?/init.lua;"..home.."/.lua50/?.lua;"..home.."/.lua50/?/init.lua;/usr/local/share/lua50/?.lua;/usr/local/share/lua50/?/init.lua;/usr/local/lib/lua50/?.lua;/usr/local/lib/lua50/?/init.lua;/usr/share/lua50/?.lua;/usr/share/lua50/?/init.lua;/usr/lib/lua50/?.lua;/usr/lib/lua50/?/init.lua"
-			LUA_CPATH = "./?.so;./l?.so;./lib?.so;"..home.."/.lua50/?.so;"..home.."/.lua50/l?.so;"..home.."/.lua50/lib?.so;/usr/local/lib/lua50/?.so;/usr/local/lib/lua50/l?.so;/usr/local/lib/lua50/lib?.so;/usr/lib/lua50/?.so;/usr/lib/lua50/l?.so;/usr/lib/lua50/lib?.so"
-		else
-			package.path = "./?.lua;./?/init.lua;"..home.."/.lua51/?.lua;"..home.."/.lua51/?/init.lua;/usr/local/share/lua51/?.lua;/usr/local/share/lua51/?/init.lua;/usr/local/lib/lua51/?.lua;/usr/local/lib/lua51/?/init.lua;/usr/share/lua51/?.lua;/usr/share/lua51/?/init.lua;/usr/lib/lua51/?.lua;/usr/lib/lua51/?/init.lua"
-			package.cpath = "./?.so;./l?.so;./lib?.so;"..home.."/.lua51/?.so;"..home.."/.lua51/l?.so;"..home.."/.lua51/lib?.so;/usr/local/lib/lua51/?.so;/usr/local/lib/lua51/l?.so;/usr/local/lib/lua51/lib?.so;/usr/lib/lua51/?.so;/usr/lib/lua51/l?.so;/usr/lib/lua51/lib?.so"
-		end
-	end
+local home = os.getenv('HOME')
+local function path()
+    return './?.lua;./?/init.lua;'..home..'/.lua50/?.lua;'..home..'/.lua50/?/init.lua;/usr/local/share/lua50/?.lua;/usr/local/share/lua50/?/init.lua;/usr/local/lib/lua50/?.lua;/usr/local/lib/lua50/?/init.lua;/usr/share/lua50/?.lua;/usr/share/lua50/?/init.lua;/usr/lib/lua50/?.lua;/usr/lib/lua50/?/init.lua'
+end
+local function cpath()
+    return './?.so;./l?.so;./lib?.so;'..home..'/.lua50/?.so;'..home..'/.lua50/l?.so;'..home..'/.lua50/lib?.so;/usr/local/lib/lua50/?.so;/usr/local/lib/lua50/l?.so;/usr/local/lib/lua50/lib?.so;/usr/lib/lua50/?.so;/usr/lib/lua50/l?.so;/usr/lib/lua50/lib?.so'
 end
 
-pcall(require, "init")
--- pcall(require, "std")
+local _,_,maj,min = string.find(_VERSION, '%s(%d)%p(%d)')
+if maj and min then
+    if tonumber(maj) >= 5 then
+        min = tonumber(min)
+
+        local home = os.getenv('HOME')
+        if min < 1 then
+            --require('compat-5.1')
+            LUA_PATH = path()
+            LUA_CPATH = cpath()
+        else
+            package.path = path()
+            package.cpath = cpath()
+        end
+    end
+end
+
+pcall(require, 'init')
+-- pcall(require, 'std')
