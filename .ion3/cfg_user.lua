03812bf0960c87f4095c1122252cd2fde5c61938
Chris Larson <clarson@kergoth.com>
2006-07-06 09:36:05 +0000

    Make gaim, firefox, and last.fm player windows show up on my floating desktop.

--- .ion3/cfg_user.lua
+++ .ion3/cfg_user.lua
@@ -2,6 +2,7 @@ dopath("look_asm-xft")
 -- dopath("look_brownsteel-xft")
 -- dopath("look_cleanviolet")
 
+
 defbindings("WMPlex", {
     bdoc("Run a terminal emulator."),
     kpress(ALTMETA.."F2", "ioncore.exec_on(_, 'urxvtc')"),
@@ -9,3 +10,24 @@ defbindings("WMPlex", {
 
 dopath("mod_statusbar")
 -- dopath("mod_dock")
+
+defwinprop{
+    class = "Gaim",
+    target = "Floating",
+}
+
+defwinprop{
+    class = "Firefox-bin",
+    target = "Floating",
+}
+
+defwinprop{
+    name = "Last.fm Player",
+    target = "Floating",
+}
+
+-- dopath("histcompl")
+dopath("detach.lua")
+detach.setup_hooks()
+
+dopath("cfg_menu")
