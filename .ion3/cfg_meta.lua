4225d45dd135855314e8a3cbc12c3e9f1db55e30
Christopher Larson <kergoth@gmail.com>
2011-11-13 10:55:42 -0800

    Drop old legacy and unused bits that are no longer part of my current setup
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- .ion3/cfg_meta.lua
+++
@@ -1,6 +0,0 @@
--- Set default modifiers. Alt should usually be mapped to Mod1 on
--- XFree86-based systems. The flying window keys are probably Mod3
--- or Mod4; see the output of 'xmodmap'.
-META="Mod4+"
--- ALTMETA="Control+"
-ALTMETA="Mod1+"
--- .lua50/std.io.lua
+++
@@ -1,7 +0,0 @@
--- Lua I/O library
-
-require "std.import"
-import "std.io.env"
-import "std.io.file"
-import "std.io.getopt"
-import "std.io.io"
