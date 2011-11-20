646198f861c1516d60851cb624d0f1d58f6751ab
Christopher Larson <kergoth@gmail.com>
2011-11-20 13:33:42 -0800

    fish: merge environment.fish into config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,50 @@
-. ~/.config/fish/environment.fish
-
 alias t 't.py --task-dir ~/Dropbox/Documents --list tasks.txt'
+
 
 set tacklebox_path ~/.config/fish/tacklebox
 set tacklebox_plugins misc python volatile z
+
+begin
+    set -l path
+    for dir in ~/bin ~/.local/bin $PATH
+        if test -d $dir
+            set path $path $dir
+        end
+    end
+    set PATH $path
+end
+
+if not set -q HOME
+    set -x HOME (cd ~; and pwd)
+end
+
+if not set -q HOSTNAME
+    set -x HOSTNAME (hostname)
+end
+
+set -x EMAIL 'kergoth@gmail.com'
+set -x FULLNAME 'Christopher Larson'
+set -x DEBEMAIL $EMAIL
+set -x DEBFULLNAME $FULLNAME
+
+set -x CVS_RSH ssh
+set -x CVSREAD yes
+
+set -x EDITOR vim
+set -x VISUAL $EDITOR
+
+set -x INPUTRC ~/.inputrc
+set -x ACKRC .ackrc
+
+set -x LC_MESSAGES C
+set -x LC_ALL
+
+set -x LESS FRX
+set -x PAGER less
+set -x MANWIDTH 80
+
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
+
 
 set OS (uname -s | tr A-Z a-z)
 if test -e ~/.config/fish/$OS.fish
