427cb706253a1487c10b7c1443cb83449b25a645
Christopher Larson <kergoth@gmail.com>
2011-11-20 13:32:46 -0800

    fish: switch to tacklebox

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@
 
 alias t 't.py --task-dir ~/Dropbox/Documents --list tasks.txt'
 
+set tacklebox_path ~/.config/fish/tacklebox
+set tacklebox_plugins misc python volatile z
+
 set OS (uname -s | tr A-Z a-z)
 if test -e ~/.config/fish/$OS.fish
     . ~/.config/fish/$OS.fish
@@ -11,7 +14,8 @@ if test -e ~/.config/fish/$HOSTNAME.fish
     . ~/.config/fish/$HOSTNAME.fish
 end
 
-. ~/.config/fish/volatile.fish
+. $tacklebox_path/tacklebox.fish
+
 
 # To ensure that I don't see a failure status in the prompt at login
 true
