4fb8d4db9850dcfc969e68aa64ea39029244c44d
Christopher Larson <kergoth@gmail.com>
2011-12-01 14:34:32 -0600

    fish: set screen title based on termtype, not env var
    The env method won't work across ssh connections, as those vars aren't allowed
    by ssh generally. This ensures you can ssh to another box and have the
    tmux/screen window title reflect the other box.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -59,7 +59,7 @@ if test $OS = darwin
 end
 
 # Functions {{{1
-if begin set -q STY; or set -q TMUX; end
+if begin test $TERM = screen; or test $TERM = screen-256color; end
     function screen_title --on-variable fish_title_string # damn hack
         printf '\033k%s\033\\' $fish_title_string
     end
