8272b3ec67cc0dda52868c21ab5c096282c0b74d
Christopher Larson <kergoth@gmail.com>
2011-12-06 22:59:02 -0700

    fish: darwin: force path to avoid ordering issue

--- config/fish/darwin.fish
+++ config/fish/darwin.fish
@@ -1,3 +1,5 @@
 set -x CLICOLOR 1
 set -x COMMAND_MODE unix2003
+
+set_path ~/bin ~/.local/bin (dirname (which fish)) /usr/local/bin /usr/local/sbin /usr/X11R6/bin /usr/bin /usr/sbin /bin /sbin
 set PATH (command ls -d ~/Library/Python/*/bin 2>/dev/null) $PATH
