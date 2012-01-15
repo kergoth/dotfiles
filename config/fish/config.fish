08a1c7c7a7eaf6422278db98eceb059c616f2b9d
Christopher Larson <kergoth@gmail.com>
2012-01-15 09:43:01 -0700

    fish: fix vim syntax issue with config.fish.
    --HG--
    extra : source : e3fb299b1c3db19be4c0439f1463ef94cb17da3c

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -60,7 +60,7 @@ end
 # Functions {{{1
 if begin test $TERM = screen; or test $TERM = screen-256color; end
     function screen_title --on-variable fish_title_string # damn hack
-        printf '\033k%s\033\\' $fish_title_string
+        printf "\033k%s\033\\" $fish_title_string
     end
 end
 
