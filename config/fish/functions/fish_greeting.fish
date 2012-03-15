0b46c2f3634dbd5d930d0e7b2efabfe344b96a4e
Christopher Larson <kergoth@gmail.com>
2012-03-15 06:46:28 -0700

    fish: don't die if ~/bin isn't in the PATH for some reason

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -2,14 +2,18 @@ set -e fish_greeting
 
 if status --is-interactive
     function fish_greeting
-        if test (t | wc -l) -ne 0
-            echo Tasks:
-            t | sed 's/^/  /'
-        end
+        if have t.py
+            if test (t | wc -l) -ne 0
+                echo Tasks:
+                t | sed 's/^/  /'
+            end
 
-        if test (h | wc -l) -ne 0
-            echo Personal tasks:
-            h | sed 's/^/  /'
+            if test (h | wc -l) -ne 0
+                echo Personal tasks:
+                h | sed 's/^/  /'
+            end
+        else
+            echo >&2 "Warning: ~/bin is not in the PATH"
         end
     end
 end
