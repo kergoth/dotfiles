b513dadb70d99563037908f0aded6e46521e04f6
Christopher Larson <kergoth@gmail.com>
2012-04-10 19:47:07 -0700

    fish: export NODE_PATH

--- config/fish/darwin.fish
+++ config/fish/darwin.fish
@@ -3,3 +3,4 @@ set -x COMMAND_MODE unix2003
 
 set_path ~/bin ~/.local/bin (dirname (which fish)) /usr/local/bin /usr/local/sbin /usr/X11R6/bin /usr/bin /usr/sbin /bin /sbin
 set PATH (command ls -d ~/Library/Python/*/bin 2>/dev/null) $PATH
+set -x NODE_PATH /usr/local/lib/node_modules
