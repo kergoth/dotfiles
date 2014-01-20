2cbb8d8dfe039b9dd75f161e1d2846d5ae5c47e3
Christopher Larson <kergoth@gmail.com>
2014-01-19 18:10:01 -0700

    att-update-env: export the vars we set
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- fish/functions/att-update-env.fish
+++ fish/functions/att-update-env.fish
@@ -1,4 +1,4 @@
-function att-update-env -d 'Update the current environment with variables from the att script'
+function att-update-env --description 'Update the current environment with variables from the att script'
     att-env | begin
         set -lx IFS =
         while read var val
@@ -6,7 +6,7 @@ function att-update-env -d 'Update the current environment with variables from t
                 set -l unsetvar (echo $var|cut -d- -f2-)
                 eval set -e $unsetvar
             else
-                eval set $var $val; or echo >&2 "Error setting "$var
+                eval set -gx $var $val; or echo >&2 "Error setting "$var
             end
         end
     end
