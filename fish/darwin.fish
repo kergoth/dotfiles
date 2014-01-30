277d9ef92746203ca105092475092f7a7b894ae3
Christopher Larson <kergoth@gmail.com>
2014-01-30 16:58:58 -0700

    darwin.fish: include ~/Library/Python/*/bin in the PATH
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- fish/darwin.fish
+++ fish/darwin.fish
@@ -1,4 +1,11 @@
 set PATH /opt/homebrew/sbin /opt/homebrew/bin $PATH
+
 if have mvim
     alias vim 'mvim -v'
+end
+
+for dir in ~/Library/Python/*/bin
+    if test -e $dir
+        set PATH $dir $PATH
+    end
 end
