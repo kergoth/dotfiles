a49d919cb0cb1ca5ab7012aa7b3bcf24f3ccd31f
Christopher Larson <kergoth@gmail.com>
2015-01-30 12:46:07 -0700

    zsh: be consistent with quoting in aliases.zsh
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,14 +2,14 @@ alias reload!='. ${ZDOTDIR:-$HOME}/.zshrc'
 alias import-global-history='fc -RI'
 
 if [[ $OSTYPE =~ darwin ]]; then
-    alias ls="ls -G"
+    alias ls='ls -G'
 else
-    alias ls="ls --color=auto"
+    alias ls='ls --color=auto'
 fi
-alias lr="ls -thl"
-alias ll="ls -hl"
-alias la="ls -Ah"
-alias l="ls -1h"
+alias lr='ls -thl'
+alias ll='ls -hl'
+alias la='ls -Ah'
+alias l='ls -1h'
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -30,7 +30,7 @@ alias t='t.py --list tasks.txt'
 alias h='t.py --list tasks-personal.txt'
 alias dtrx='dtrx --one=here'
 alias mosh='mosh --forward-agent'
-#alias mosh="perl -E 'print \"\e[?1005h\e[?1002h\"'; mosh"
+#alias mosh="perl -E 'print \'\e[?1005h\e[?1002h\''; mosh"
 alias smem='smem -k'
 alias ytdl=youtube-dl
 alias pipsi='pipsi --home="$WORKON_HOME"'
@@ -43,7 +43,7 @@ if (( $+commands[pacman-color] )); then
     alias pacman='pacman-color'
 fi
 
-alias ag="ag -S --pager=${PAGER:-less}"
+alias ag='ag -S --pager=${PAGER:-less}'
 if (( $+commands[ag] )); then
     alias bbag="ag -G '\.(bb|bbappend|inc|conf)$'"
 elif (( $+commands[ack] )); then
@@ -53,25 +53,25 @@ elif (( $+commands[ack] )); then
 fi
 
 if [[ $OSTYPE =~ darwin ]]; then
-    alias ps="ps ux"
+    alias ps='ps ux'
     if (( $+commands[dfc] )); then
         alias df=dfc
-        alias dfc="dfc -T"
+        alias dfc='dfc -T'
     fi
-    alias locate="mdfind -name"
-    alias vim="mvim -v"
-    alias plaincopy="pbpaste -Prefer txt | pbcopy; pbpaste; echo"
+    alias locate='mdfind -name'
+    alias vim='mvim -v'
+    alias plaincopy='pbpaste -Prefer txt | pbcopy; pbpaste; echo'
     alias drop=trash
 else
-    alias ps="ps fux"
+    alias ps='ps fux'
     if (( $+commands[dfc] )); then
         alias df=dfc
-        alias dfc="dfc -T -q mount -p -rootfs"
+        alias dfc='dfc -T -q mount -p -rootfs'
     fi
-    alias rm="rm --one-file-system"
+    alias rm='rm --one-file-system'
     alias drop=bgrm
 fi
 
 # Global
-alias -g G="| grep"
-alias -g L="| less"
+alias -g G='| grep'
+alias -g L='| less'
