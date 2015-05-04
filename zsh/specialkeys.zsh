0b8e4271ab78a95ac9d8b3baf76e17f31155bd2a
Christopher Larson <kergoth@gmail.com>
2015-05-04 13:25:55 -0700

    zsh: add compat fallback up/down keybinds
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/specialkeys.zsh
+++ zsh/specialkeys.zsh
@@ -32,6 +32,11 @@ key[PageDown]=${terminfo[knp]}
 bindkey "^[^[[D" backward-word
 bindkey "^[^[[C" forward-word
 
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
 # bind P and N for EMACS mode
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
