2781df137a7d47217540f85bed489105968d4146
Christopher Larson <kergoth@gmail.com>
2015-07-08 11:36:21 -0700

    node: install 'globally' into XDG_DATA_HOME
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ node/node.zsh
@@ -0,0 +1,7 @@
+NPM_PACKAGES="$XDG_DATA_HOME/npm"
+
+typeset -gxT NODE_PATH="$NODE_PATH" node_path
+node_path=($NPM_PACKAGES/lib/node_modules $node_path)
+
+path=($NPM_PACKAGES/bin $path)
+manpath=($NPM_PACKAGES/share/man $manpath)
