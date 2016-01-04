7e8b64ff7e1f99e139d42f798f2ba834bb949b3c
Christopher Larson <kergoth@gmail.com>
2016-01-04 16:51:27 -0700

    less: drop -S
    I want lines wrapped, not chopped with horizontal scrolling. This also fixes
    the quit-if-one-screen behavior.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- less/less.zsh
+++ less/less.zsh
@@ -1,7 +1,7 @@
 # Set the default Less options.
 # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
 # Remove -X and -F (exit if the content fits on one screen) to enable it.
-export LESS='-F -g -i -M -R -S -w -X -z-4'
+export LESS='-F -g -i -M -R -w -X -z-4'
 
 export LESSOPEN="|mylesspipe.sh %s"
 if (( $+commands[pygmentize] )); then
