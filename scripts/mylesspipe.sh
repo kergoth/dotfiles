48f383e3836047e0651dffde3fd5581e29e553d6
Christopher Larson <kergoth@gmail.com>
2016-02-15 19:26:53 -0700

    mylesspipe.sh: use diff-so-fancy/diff-highlight
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- scripts/mylesspipe.sh
+++ scripts/mylesspipe.sh
@@ -6,5 +6,12 @@ case "$1" in
             exec mdless --no-pager "$1"
         fi
         ;;
+    *.diff|*.patch)
+        if which diff-so-fancy >/dev/null 2>&1; then
+            exec lesspipe.sh "$1" | diff-so-fancy
+        elif which diff-highlight >/dev/null 2>&1; then
+            exec lesspipe.sh "$1" | diff-highlight
+        fi
+        ;;
 esac
 exec lesspipe.sh "$1"
