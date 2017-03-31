91361b8f29d8dc0f25bb69e182d1255fd6cbff75
Christopher Larson <kergoth@gmail.com>
2017-03-31 09:12:31 -0700

    python: postpone pyenv init to speed up zsh startup
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,3 +1,15 @@
+# Load pyenv on demand, to speed up shell startup
 if (( $+commands[pyenv] )); then
-    eval "$(pyenv init -)"
+    export PATH="$HOME/.pyenv/shims:$PATH"
+
+    pyenv () {
+        eval "$(command pyenv init -)"
+        pyenv "$@"
+    }
+
+    compctl -K _pyenv pyenv
+    _pyenv () {
+        eval "$(command pyenv init -)"
+        _pyenv "$@"
+    }
 fi
