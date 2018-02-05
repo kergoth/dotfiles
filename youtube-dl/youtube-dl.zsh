f39b68b203ce031610597e551cdc8406013b9083
Christopher Larson <kergoth@gmail.com>
2018-02-05 12:54:14 -0700

    youtube-dl: add youtube-dl-playlist/ytdl-playlist aliases
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- youtube-dl/youtube-dl.zsh
+++ youtube-dl/youtube-dl.zsh
@@ -1,2 +1,4 @@
-alias ytdl=youtube-dl
 alias youtube-dl='youtube-dl --no-playlist'
+alias youtube-dl-playlist="-o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' -i"
+alias ytdl=youtube-dl
+alias ytdl-playlist=youtube-dl-playlist
