580987e856ee3f14a5a6ba8b747aa3e06c148408
Christopher Larson <kergoth@gmail.com>
2018-02-06 15:09:51 -0700

    oni: add configuration
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ oni/config.js
@@ -0,0 +1,36 @@
+// https://github.com/onivim/oni/wiki/Configuration
+
+const activate = (oni) => {
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    // Or remove the default bindings here by uncommenting the below line:
+    // oni.input.unbind("<c-p>")
+}
+
+const deactivate = () => {
+    console.log("config deactivated")
+}
+
+module.exports = {
+    activate,
+    deactivate,
+
+    "oni.loadInitVim": true,
+    //"oni.bookmarks": ["~/Documents"],
+
+    "editor.fontSize": "14px",
+    //"editor.fontFamily": "Input Mono Narrow",
+    "editor.fontFamily": "InputMono-Thin",
+
+    "environment.additionalPaths": [
+        "/Users/kergoth/Dropbox/dotfiles/scripts",
+        "/Users/kergoth/.local/bin",
+        "/opt/homebrew/bin",
+        "/usr/local/bin",
+        "/usr/bin"
+    ],
+
+    "ui.colorscheme": "base16-tomorrow-night",
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto"
+}
