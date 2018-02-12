9757265c18d09e80d43f65effc89db95de0a9864
Christopher Larson <kergoth@gmail.com>
2018-02-12 13:20:44 -0700

    ytdl: add youtube-dl script that handles pocket links specially
    Uses pocket-search, which uses pocket-cli, to get the actual underlying
    link for pocket read urls.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- youtube-dl/youtube-dl.zsh
+++ youtube-dl/youtube-dl.zsh
@@ -1,4 +1,1 @@
-alias youtube-dl='youtube-dl --no-playlist'
-alias youtube-dl-playlist="-o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' -i"
-alias ytdl=youtube-dl
-alias ytdl-playlist=youtube-dl-playlist
+alias ytdl-playlist="ytdl -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' -i"
