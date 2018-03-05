f52102cc8b70e5f08088c928757d731a5b5bedda
Christopher Larson <kergoth@gmail.com>
2018-03-05 13:01:55 -0700

    python/pyenv: move to XDG_DATA_HOME, run virtualenv-init
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,11 +1,9 @@
-# Load pyenv on demand, to speed up shell startup
-if [[ -d ~/.pyenv ]]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-fi
+export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
 if (( $+commands[pyenv] )); then
     eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init - 2>/dev/null)"
 
     for i in $(pyenv root)/completions/pyenv.zsh \
              ${HOMEBREW_PREFIX:-/Users/Shared/homebrew}/opt/pyenv/share/zsh/site-functions/pyenv.zsh; do
