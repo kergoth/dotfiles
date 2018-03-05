7d976f897a0d4ecb476a3e51d5a165eef8f023a0
Christopher Larson <kergoth@gmail.com>
2018-03-05 12:56:54 -0700

    node/npm: obey XDG paths
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- node/node.zsh
+++ node/node.zsh
@@ -4,3 +4,5 @@ typeset -gxT NODE_PATH="$NODE_PATH" node_path
 node_path=($NPM_PACKAGES/lib/node_modules $node_path)
 
 path=($NPM_PACKAGES/bin $path)
+
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
