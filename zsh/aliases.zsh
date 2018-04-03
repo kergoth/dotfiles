cb4e777296f6da04e95e9758e63ce1f5398a48a0
Christopher Larson <kergoth@gmail.com>
2018-04-03 10:52:36 -0700

    zsh: add rezsh/relogin aliases
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,6 +22,8 @@ alias diff='diff -urNd'
 alias bc='bc -ql'
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias xz='xz --threads=0'
+alias rezsh='exec zsh --login'
+alias relogin='su - $USER'
 
 what () {
     tldr "$1" || cheat "$1" || man "$1"
