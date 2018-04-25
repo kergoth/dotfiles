ebb18f60c6476620d303df5f15a99f4e69fedd76
Christopher Larson <kergoth@gmail.com>
2018-04-25 12:35:44 -0700

    ls: drop OTHER_WRITABLE formatting on WSL
    Directories usually end up 777 there, particularly under drvfs, so don't
    even bother formatting those specially.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- ls/ls.zsh
+++ ls/ls.zsh
@@ -1,14 +1,15 @@
+DIRCOLORS=$XDG_CONFIG_HOME/dir_colors
 if [[ $OSTYPE =~ darwin ]]; then
     if (( $+commands[gls] )); then
         alias ls='gls --color=auto'
-        eval "$(gdircolors -b $DOTFILESDIR/ls/dir_colors)"
+        eval "$(gdircolors -b $DIRCOLORS)"
     else
         alias ls='ls -G'
     fi
     export LSCOLORS="$(cat $DOTFILESDIR/ls/bsd_colors)"
 else
     alias ls='ls --color=auto'
-    eval "$(dircolors -b $DOTFILESDIR/ls/dir_colors)"
+    eval "$(dircolors -b $DIRCOLORS)"
 fi
 
 alias lr='ls -thl'
