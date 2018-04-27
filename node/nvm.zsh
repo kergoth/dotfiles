48d1cc7df1f6fc56b2690f25761b471d999b0bc1
Christopher Larson <kergoth@gmail.com>
2018-04-27 09:45:31 -0700

    node: restore unintentional removed conditional
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,6 +1,8 @@
-if [ -n "$NODE_VERSION" ]; then
-    . "$NVM_DIR/nvm.sh" --no-use
-    path=($NVM_DIR/versions/node/$NODE_VERSION/bin $path)
-else
-    . "$NVM_DIR/nvm.sh"
+if [[ -s "$NVM_DIR/nvm.sh" ]]; then
+    if [[ -n "$NODE_VERSION" ]]; then
+        . "$NVM_DIR/nvm.sh" --no-use
+        path=($NVM_DIR/versions/node/$NODE_VERSION/bin $path)
+    else
+        . "$NVM_DIR/nvm.sh"
+    fi
 fi
