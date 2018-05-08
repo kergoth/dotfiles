3cc91df3a072e73ef0f571f10b893743aba68dcd
Christopher Larson <kergoth@gmail.com>
2018-05-08 12:53:49 -0700

    fzf: use fd if available
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,4 +1,8 @@
-export FZF_DEFAULT_COMMAND='ag -g ""'
+if (( $+commands[fd] )); then
+    export FZF_DEFAULT_COMMAND='fd -t f ""'
+else
+    export FZF_DEFAULT_COMMAND='ag -g ""'
+fi
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 . $DOTFILESDIR/fzf/themes/base16-tomorrow-night.config
