f79ce584e8e9d7ef84bc882cc922981c34718d52
Christopher Larson <chris_larson@mentor.com>
2018-05-09 21:40:14 +0500

    fzf: show color when using fd
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,8 +1,9 @@
+. $DOTFILESDIR/fzf/themes/base16-tomorrow-night.config
+
 if (( $+commands[fd] )); then
-    export FZF_DEFAULT_COMMAND='fd -t f ""'
+    export FZF_DEFAULT_COMMAND='fd -c always -t f ""'
+    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --ansi"
 else
-    export FZF_DEFAULT_COMMAND='ag -g ""'
+    export FZF_DEFAULT_COMMAND='ag --color -g ""'
 fi
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-. $DOTFILESDIR/fzf/themes/base16-tomorrow-night.config
