b46f182363ee5c54cc1f7c66e3384b40f169033e
Christopher Larson <kergoth@gmail.com>
2018-05-09 09:42:10 -0700

    fzf: also use fd for alt-c
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -2,6 +2,7 @@
 
 if (( $+commands[fd] )); then
     export FZF_DEFAULT_COMMAND='fd -c always -t f ""'
+    export FZF_ALT_C_COMMAND='fd -c always -t d ""'
     export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --ansi"
 else
     export FZF_DEFAULT_COMMAND='ag --color -g ""'
