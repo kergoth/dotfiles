b5c7bf5b34df59be1173e22ef6b5b183542c4a07
Christopher Larson <kergoth@gmail.com>
2018-05-17 13:07:19 -0700

    zsh: don't know failure exit code in prompt when there's no command
    For example, when I ^C at the prompt.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ zsh/prompt.zsh
@@ -0,0 +1,22 @@
+# Don't show a failure exit code in the prompt when no command was run,
+# for example when I ^C at the prompt
+
+promptline_lastcmd=
+
+promptline_begin() {
+	promptline_lastcmd=$1
+}
+add-zsh-hook preexec promptline_begin
+
+promptline_last_code_fixup() {
+    if [ -z "$promptline_lastcmd" ]; then
+        PROMPTLINE_LAST_EXIT_CODE=0
+    else
+        PROMPTLINE_LAST_EXIT_CODE=
+    fi
+    promptline_lastcmd=
+}
+
+if [[ ! ${precmd_functions[(r)promptline_last_code_fixup]} == promptline_last_code_fixup ]]; then
+    precmd_functions=(promptline_last_code_fixup $precmd_functions)
+fi
