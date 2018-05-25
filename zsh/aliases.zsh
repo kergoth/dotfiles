c4642a42d9c99667a602d4992e904f9646dfa9b8
Christopher Larson <kergoth@gmail.com>
2018-05-25 08:21:33 -0700

    vim: only pass --servername if vim supports it
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -96,7 +96,7 @@ if (( $+commands[vim] )); then
     alias vi=vim
 fi
 
-if [[ -n $VIM_SERVER ]]; then
+if [[ -n $VIM_SERVER ]] && vim --version 2>/dev/null | grep -qw '+clientserver'; then
     alias vim="vim --servername $VIM_SERVER --remote"
 fi
 
