2b091a16463a32f3a6a1a79679628e74d35957db
Christopher Larson <kergoth@gmail.com>
2018-06-01 14:36:15 -0700

    pyenv: lazy init to speed zsh load time
    `pyenv init -` runs a `pyenv hash`, which is slow. Keep the
    virtualenv-init, as it installs a precmd hook, and it doesn't run
    a rehash.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,11 +1,10 @@
 export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
 
 if (( $+commands[pyenv] )); then
-    eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init - 2>/dev/null)"
 
-    for i in $(pyenv root)/completions/pyenv.zsh \
+    for i in $PYENV_ROOT/completions/pyenv.zsh \
              ${HOMEBREW_PREFIX:-/opt/homebrew}/opt/pyenv/share/zsh/site-functions/pyenv.zsh; do
         if [[ -f $i ]]; then
             . $i
