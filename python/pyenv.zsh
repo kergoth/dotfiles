bb0d32a56b31917c7da7a8330c1145fc2920a3e7
Christopher Larson <kergoth@gmail.com>
2018-06-14 09:21:01 -0700

    python: kill pyenv-virtualenv usage, I don't use it
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -2,8 +2,6 @@ export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
 export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
 
 if (( $+commands[pyenv] )); then
-    eval "$(pyenv virtualenv-init - 2>/dev/null)"
-
     for i in $PYENV_ROOT/completions/pyenv.zsh \
              ${HOMEBREW_PREFIX:-/opt/homebrew}/opt/pyenv/share/zsh/site-functions/pyenv.zsh; do
         if [[ -f $i ]]; then
