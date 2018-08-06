f16ff693dbb87921ae1c160e7cb754f18b89aede
Christopher Larson <kergoth@gmail.com>
2018-08-06 09:19:31 -0700

    less: drop lesspipe/pygmentize bits
    Now that I'm using bat, this is unnecessary and just slows down less
    when I do want to use it instead.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- less/less.zsh
+++ less/less.zsh
@@ -2,11 +2,3 @@
 # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
 # Remove -X and -F (exit if the content fits on one screen) to enable it.
 export LESS='-F -g -i -M -R -w -X -z-4'
-
-export LESSOPEN="|mylesspipe.sh %s"
-if (( $+commands[pygmentize] )); then
-    # If we have pygmentize, prefer it over code2color
-    export LESSCOLORIZER=mypygmentize
-    export PYGMENTIZE_FORMATTER=terminal
-    export PYGMENTIZE_STYLE=base16-tomorrow-night
-fi
