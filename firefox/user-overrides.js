45f1b9a6df36828cb195372d93c1fbb1ef2a8230
Christopher Larson <kergoth@gmail.com>
2018-08-23 08:35:07 -0700

    firefox: set more rfp alternative settings
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -81,8 +81,6 @@ user_pref("network.cookie.lifetimePolicy", 3);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 /* 4001: disable First Party Isolation (FF51+), as this causes breakage for me */
 user_pref("privacy.firstparty.isolate", false);
-/* 4501: disable privacy.resistFingerprinting (FF41+), it's not worth forcing my window size */
-user_pref("privacy.resistFingerprinting", false); // (hidden pref) (not hidden FF55+)
 /* 5003: enable closing browser with last tab ***/
 user_pref("browser.tabs.closeWindowWithLastTab", true);
 /* 5010: disable ctrl-tab previews, as this also switches to MRU order ***/
@@ -92,3 +90,73 @@ user_pref("browser.ctrlTab.previews", false);
  * true: load the new tab in the background, leaving focus on the current tab
  * false: load the new tab in the foreground, taking the focus from the current tab. ***/
 user_pref("browser.tabs.loadDivertedInBackground", true);
+
+// RFP
+/* 4501: disable privacy.resistFingerprinting (FF41+), it's not worth forcing my window size */
+user_pref("privacy.resistFingerprinting", false); // (hidden pref) (not hidden FF55+)
+// FF55+
+// 4601: [2514] spoof (or limit?) number of CPU cores (FF48+)
+   // [WARNING] *may* affect core chrome/Firefox performance, will affect content.
+   // [1] https://bugzilla.mozilla.org/1008453
+   // [2] https://trac.torproject.org/projects/tor/ticket/21675
+   // [3] https://trac.torproject.org/projects/tor/ticket/22127
+   // [4] https://html.spec.whatwg.org/multipage/workers.html#navigator.hardwareconcurrency
+   // user_pref("dom.maxHardwareConcurrency", 2);
+// * * * /
+// FF56+
+// 4602: [2411] disable resource/navigation timing
+user_pref("dom.enable_resource_timing", false);
+// 4603: [2412] disable timing attacks
+   // [1] https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
+user_pref("dom.enable_performance", false);
+// 4604: [2512] disable device sensor API
+   // [WARNING] [SETUP] Optional protection depending on your device
+   // [1] https://trac.torproject.org/projects/tor/ticket/15758
+   // [2] https://blog.lukaszolejnik.com/stealing-sensitive-browser-data-with-the-w3c-ambient-light-sensor-api/
+   // [3] https://bugzilla.mozilla.org/buglist.cgi?bug_id=1357733,1292751
+   // user_pref("device.sensors.enabled", false);
+// 4605: [2515] disable site specific zoom
+   // Zoom levels affect screen res and are highly fingerprintable. This does not stop you using
+   // zoom, it will just not use/remember any site specific settings. Zoom levels on new tabs
+   // and new windows are reset to default and only the current tab retains the current zoom
+user_pref("browser.zoom.siteSpecific", false);
+// 4606: [2501] disable gamepad API - USB device ID enumeration
+   // [WARNING] [SETUP] Optional protection depending on your connected devices
+   // [1] https://trac.torproject.org/projects/tor/ticket/13023
+   // user_pref("dom.gamepad.enabled", false);
+// 4607: [2503] disable giving away network info (FF31+)
+   // e.g. bluetooth, cellular, ethernet, wifi, wimax, other, mixed, unknown, none
+   // [1] https://developer.mozilla.org/docs/Web/API/Network_Information_API
+   // [2] https://wicg.github.io/netinfo/
+   // [3] https://bugzilla.mozilla.org/960426
+user_pref("dom.netinfo.enabled", false);
+// 4608: [2021] disable the SpeechSynthesis (Text-to-Speech) part of the Web Speech API
+   // [1] https://developer.mozilla.org/docs/Web/API/Web_Speech_API
+   // [2] https://developer.mozilla.org/docs/Web/API/SpeechSynthesis
+   // [3] https://wiki.mozilla.org/HTML5_Speech_API
+user_pref("media.webspeech.synth.enabled", false);
+// * * * /
+// FF57+
+// 4610: [2506] disable video statistics - JS performance fingerprinting (FF25+)
+   // [1] https://trac.torproject.org/projects/tor/ticket/15757
+   // [2] https://bugzilla.mozilla.org/654550
+user_pref("media.video_stats.enabled", false);
+// 4611: [2509] disable touch events
+   // fingerprinting attack vector - leaks screen res & actual screen coordinates
+   // 0=disabled, 1=enabled, 2=autodetect
+   // [WARNING] [SETUP] Optional protection depending on your device
+   // [1] https://developer.mozilla.org/docs/Web/API/Touch_events
+   // [2] https://trac.torproject.org/projects/tor/ticket/10286
+   // user_pref("dom.w3c_touch_events.enabled", 0);
+// * * * /
+// FF59+
+// 4612: [2511] disable MediaDevices change detection (FF51+)
+   // [1] https://developer.mozilla.org/docs/Web/Events/devicechange
+   // [2] https://developer.mozilla.org/docs/Web/API/MediaDevices/ondevicechange
+user_pref("media.ondevicechange.enabled", false);
+// * * * /
+// FF60+
+// 4613: [2011] disable WebGL debug info being available to websites
+   // [1] https://bugzilla.mozilla.org/1171228
+   // [2] https://developer.mozilla.org/docs/Web/API/WEBGL_debug_renderer_info
+user_pref("webgl.enable-debug-renderer-info", false);
