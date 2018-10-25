129ef1898dd87b76ae2c6868980170221f131b26
Christopher Larson <kergoth@gmail.com>
2018-10-25 11:28:53 -0700

    ruby: add rbenv bits
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ ruby/path.zsh
@@ -0,0 +1,1 @@
+path=(~/.gem/ruby/*/bin(Nn[-1]) $path)
--- ruby/ruby.zsh
+++
@@ -1,7 +0,0 @@
-path=(~/.gem/ruby/*/bin(Nn[-1]) $path)
-
-export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
-export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
-export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
-export GEM_HOME="$XDG_DATA_HOME"/gem
-export GEM_SPEC_CACHE="$XDG_CACHE_HOME"/gem
