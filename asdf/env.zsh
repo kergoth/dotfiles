9421e2e0d9c9b56e951721e13dfacb51e652c5ea
Christopher Larson <kergoth@gmail.com>
2018-11-16 08:33:31 -0700

    asdf: set ASDF_CONFIG_FILE
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ asdf/env.zsh
@@ -0,0 +1,2 @@
+export ASDF_DATA_DIR="${ASDF_DATA_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/asdf}"
+export ASDF_CONFIG_FILE="${ASDF_CONFIG_FILE:-${XDG_CONFIG_HOME:-$HOME/.config}/asdfrc}"
--- asdf/path.zsh
+++ asdf/path.zsh
@@ -1,2 +1,1 @@
-export ASDF_DATA_DIR="${ASDF_DATA_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/asdf}"
 PATH="$ASDF_DATA_DIR/bin:$ASDF_DATA_DIR/shims:$PATH"
