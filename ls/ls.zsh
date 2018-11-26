17d131f338a0740769b478b0207f4b7476252a57
Christopher Larson <kergoth@gmail.com>
2018-11-26 13:52:55 -0700

    Use exa if available
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- ls/ls.zsh
+++ ls/ls.zsh
@@ -1,18 +1,25 @@
 DIRCOLORS=$XDG_CONFIG_HOME/dir_colors
-if [[ $OSTYPE =~ darwin ]]; then
+if (( $+commands[exa] )); then
+    alias exa='exa --colour-scale'
+    alias ls=exa
+    alias la='exa -a'
+    alias lr='exa -l -s modified'
+elif [[ $OSTYPE =~ darwin ]]; then
     if (( $+commands[gls] )); then
-        alias ls='gls --color=auto'
+        alias ls='gls --color=auto -h'
         eval "$(gdircolors -b $DIRCOLORS)"
     else
-        alias ls='ls -G'
+        alias ls='ls -G -h'
     fi
     export LSCOLORS="$(cat $DOTFILESDIR/ls/bsd_colors)"
+    alias la='ls -A'
+    alias lr='ls -lt'
 else
-    alias ls='ls --color=auto'
     eval "$(dircolors -b $DIRCOLORS)"
+    alias ls='ls --color=auto -h'
+    alias la='ls -A'
+    alias lr='ls -lt'
 fi
 
-alias lr='ls -thl'
-alias ll='ls -hl'
-alias la='ls -Ah'
-alias l='ls -1h'
+alias ll='ls -l'
+alias l='ls -1'
