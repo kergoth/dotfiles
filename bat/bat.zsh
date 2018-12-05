dc07125a6b217ee24bd9fb7b028925289982bd33
Christopher Larson <kergoth@gmail.com>
2018-12-04 18:37:32 -0700

    bat: only set up if bat is installed
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- bat/bat.zsh
+++ bat/bat.zsh
@@ -1,3 +1,5 @@
-BAT_THEME=base16-tomorrow-night
-alias bat="bat ${BAT_THEME+--theme=$BAT_THEME}"
-LESSCOLORIZER="${LESSCOLORIZER:-bat --decorations never}"
+if (( $+commands[bat] )); then
+    BAT_THEME=base16-tomorrow-night
+    alias bat="bat ${BAT_THEME+--theme=$BAT_THEME}"
+    LESSCOLORIZER="${LESSCOLORIZER:-bat --decorations never}"
+fi
