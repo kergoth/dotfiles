3c66854c63eedfd2cbe7291da338a28ff9b696e9
Christopher Larson <kergoth@gmail.com>
2019-02-15 12:44:45 -0700

    bat: update for current version
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- bat/bat.zsh
+++ bat/bat.zsh
@@ -1,5 +1,3 @@
 if (( $+commands[bat] )); then
-    BAT_THEME=base16-tomorrow-night
-    alias bat="bat ${BAT_THEME+--theme=$BAT_THEME}"
     LESSCOLORIZER="${LESSCOLORIZER:-bat --decorations never}"
 fi
