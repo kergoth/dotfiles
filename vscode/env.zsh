657ff133b7145245f350e7f12b468cd4ae94ecac
Christopher Larson <kergoth@gmail.com>
2019-09-16 13:55:33 -0700

    Fix the ability to use the 'code' command in tmux with Remote - SSH
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ vscode/env.zsh
@@ -0,0 +1,9 @@
+if [[ -n "$SSH_AUTH_SOCK" ]] && (( $+commands[code] )); then
+    code_dir="$(dirname "$(command -v code)")"
+    export VSCODE_SERVER_DIR=
+    case "$code_dir" in
+        "$HOME/.vscode-server/"*)
+            VSCODE_SERVER_DIR="$(dirname "$code_dir")"
+            ;;
+    esac
+fi
