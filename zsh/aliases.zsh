146d6eaa5e9507455b621fbdf5bbbbfb0fc9d820
Christopher Larson <kergoth@gmail.com>
2019-10-02 12:04:32 -0700

    Fix broken parallel alias
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,7 +21,6 @@ alias dtrx='dtrx --one=here'
 alias mosh='mosh --forward-agent'
 #alias mosh="perl -E 'print \'\e[?1005h\e[?1002h\''; mosh"
 alias smem='smem -k'
-alias parallel='parallel --bibtex'
 alias shfmt='shfmt -i 4 -ci -bn -s'
 alias mr='mr -d "$(find_up .mrconfig || echo .)"'
 alias sshnew='ssh -o "ControlPath none"'
