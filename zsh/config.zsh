551e6d0182a550adfe1d776585500dfc0bc9993f
Christopher Larson <kergoth@gmail.com>
2019-11-22 08:28:32 -0700

    zsh: enable the git fast-syntax-highlighting chroma
    This chroma will highlight too-long commit messages being passed to `git
    commit -m`, which is about the only useful case of chroma I've seen in
    my personal workflow so far. Unfortunately, it doesn't work for my `cm`
    or `cma` aliases at this time, see
    zdharma/fast-syntax-highlighting#164.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -68,8 +68,11 @@ ZSH_AUTOSUGGEST_USE_ASYNC=1
 # Move prioritize-cwd-history into xdg path
 ZSH_PRIORITIZE_CWD_HISTORY_DIR=$XDG_DATA_HOME/zsh/prioritize-cwd-history
 
-# Kill fast-syntax-highlighting chroma funcs
-FAST_HIGHLIGHT=()
+# Kill most fast-syntax-highlighting chroma funcs
+FAST_HIGHLIGHT=(
+  chroma-git           :chroma/main-chroma.ch%git
+  chroma-hub           :chroma/-hub.ch
+)
 
 if autoload -Uz bracketed-paste-magic; then
     zle -N bracketed-paste bracketed-paste-magic
