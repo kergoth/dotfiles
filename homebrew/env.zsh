5c9b47c8098526315a4044801000d99935dab96e
Christopher Larson <kergoth@gmail.com>
2019-12-03 08:22:00 -0700

    homebrew: use ~/.brew
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,6 +1,2 @@
 export HOMEBREW_AUTO_UPDATE=1
-if [[ "$OSTYPE" == darwin* ]]; then
-    export HOMEBREW_PREFIX=/opt/homebrew
-else
-    export HOMEBREW_PREFIX=~/.brew
-fi
+export HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-~/.brew}
