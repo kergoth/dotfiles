2a369d8e95db21094bb010071b22c6545236b5e7
Christopher Larson <kergoth@gmail.com>
2019-12-17 08:51:32 -0700

    homebrew: add bits to handle a dual-install for admin vs non-admin
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,2 +1,14 @@
 export HOMEBREW_AUTO_UPDATE=1
 export HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-~/.brew}
+
+# Optional structure for a dual-homebrew setup for admin vs non-admin
+case "$OSTYPE" in
+    darwin*)
+        export ADMIN_HOMEBREW_PREFIX="${ADMIN_HOMEBREW_PREFIX:-/opt/homebrew}"
+        case "$LOGNAME" in
+            "$OSX_ADMIN_LOGNAME")
+                export HOMEBREW_PREFIX="$ADMIN_HOMEBREW_PREFIX"
+                ;;
+        esac
+        ;;
+esac
