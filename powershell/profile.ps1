b98976477c900dbdb66d3fbdd49d57afd72b2d72
Christopher Larson <kergoth@gmail.com>
2020-01-21 10:27:40 -0700

    powershell: use and configure PSReadLine
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,1 +1,31 @@
 Import-Module DirColors
+
+Import-Module PSReadLine
+
+# I prefer emacs readline behavior
+Set-PSReadLineOption -EditMode Emacs
+
+# Search history with arrows
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# In Emacs mode - Tab acts like in bash, but the Windows style completion
+# is still useful sometimes, so bind some keys so we can do both
+Set-PSReadLineKeyHandler -Key Ctrl+q -Function TabCompleteNext
+Set-PSReadLineKeyHandler -Key Ctrl+Q -Function TabCompletePrevious
+
+# Clipboard interaction is bound by default in Windows mode, but not Emacs mode.
+Set-PSReadLineKeyHandler -Key Ctrl+C -Function Copy
+Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
+
+# The built-in word movement uses character delimiters, but token based word
+# movement is also very useful - these are the bindings you'd use if you
+# prefer the token based movements bound to the normal emacs word movement
+# key bindings.
+Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
+Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
+Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
