28af58bf5859080abaed0553cdbc1bf2bedd675d
Christopher Larson <kergoth@gmail.com>
2020-02-12 11:08:28 -0700

    powershell: add cargo bindir to Path
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -84,7 +84,7 @@ if (Get-Module -Listavailable -Name posh-alias)
     Add-Alias Reload-Profile '& $profile'
 }
 
-# Add pyenv-win to the Path if we have it. Note: can use setx to set it in the registry instead.
+$env:Path += ";$env:USERPROFILE/.cargo/bin"
 if (Test-Path "$env:USERPROFILE/.pyenv")
 {
     $env:PYENV = "$env:USERPROFILE/.pyenv/pyenv-win"
