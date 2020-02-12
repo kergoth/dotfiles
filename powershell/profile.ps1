db2288ec720583bc4ddc35dfb4d563cfb13b4840
Christopher Larson <kergoth@gmail.com>
2020-02-12 10:09:10 -0700

    powershell: use posh when installed
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,5 +1,22 @@
 Import-Module PSReadLine
 Import-Module Recycle
+
+# Dracula Prompt configuration
+if (Get-Module -Listavailable -Name posh-git)
+{
+    Import-Module posh-git
+    $GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+    $GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+    $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+    # Dracula Git Status Configuration
+    $GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+    $GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+    $GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+}
+
+# 'z'. Always import it after prompt setup.
 Import-Module ZLocation
 
 # Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
