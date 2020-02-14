9b7f2b17c8df88d657f749f3f90a9af0fb15a8be
Christopher Larson <kergoth@gmail.com>
2020-02-14 10:20:56 -0700

    alacritty: drop
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- alacritty/alacritty.zsh
+++
@@ -1,8 +0,0 @@
-if [[ $OSTYPE =~ darwin ]]; then
-    alacritty() {
-        local app=$(locate_app_by_id io.alacritty | head -n 1)
-        "$app/Contents/MacOS/alacritty" --working-directory "$PWD" "$@"
-    }
-else
-    alias alacritty='alacritty --working-directory "$PWD"'
-fi
