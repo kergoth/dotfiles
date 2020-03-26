3848fa1c66d6c5b82cc8c7351f38355cc7e8f2ce
Christopher Larson <kergoth@gmail.com>
2020-03-26 08:32:28 -0700

    less: don't rely on $commands in env.zsh
    env.zsh is loaded before path.zsh, so we can't rely on the binaries being
    available at that point. I considered changing the load order, but often
    variables defined by env.zsh are needed and used in the PATH, so that's not
    ideal either.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- less/env.zsh
+++ less/env.zsh
@@ -1,4 +1,1 @@
 export LESSHISTFILE=$XDG_DATA_HOME/less/lesshist
-if (( $+commands[less] )); then
-    export PAGER=less
-fi
--- less/less.zsh
+++ less/less.zsh
@@ -2,3 +2,7 @@
 # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
 # Remove -X and -F (exit if the content fits on one screen) to enable it.
 export LESS='-F -g -i -M -R -w -X -z-4'
+
+if (( $+commands[less] )); then
+    export PAGER=less
+fi
