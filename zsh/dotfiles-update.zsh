6ebd4dbd1e58807855ec77281fe0aa0d01df3389
Christopher Larson <kergoth@gmail.com>
2020-04-03 08:30:22 -0700

    Split out script/sync
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/dotfiles-update.zsh
+++ zsh/dotfiles-update.zsh
@@ -1,22 +1,7 @@
 if [[ -z $DOTFILES_NO_UPDATE ]]; then
     DOTFILES_STAMP=$XDG_CACHE_HOME/zsh/dotfiles-updated
-    if [[ -n $DOTFILES_STAMP(#qN.mh+24) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
-        (
-            cd $DOTFILESDIR || exit 1
-            if [ -e .mrconfig ]; then
-                echo >&2 "Checking for dotfiles updates (once daily)"
-                mr update
-            elif [ -e .git ]; then
-                echo >&2 "Checking for dotfiles updates (once daily)"
-                git pull
-                if [ -e vim/.git ]; then
-                    ( cd vim && git pull )
-                fi
-            fi
-            if [[ -e vim/script/bootstrap ]]; then
-                ./vim/script/bootstrap || :
-            fi
-        )
-        touch $DOTFILES_STAMP
+    if [[ -e "$DOTFILESDIR/.git" ]] && [[ -n $DOTFILES_STAMP(#qN.mh+24) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
+        echo >&2 "Checking for dotfiles updates (once daily)"
+        "$DOTFILESDIR"/script/sync
     fi
 fi
