47fa85057a62bc702f6e3a4721cd9970d962bfc1
Christopher Larson <kergoth@gmail.com>
2020-04-04 16:35:01 -0700

    homebrew: install my casks to ~/Applications
    If I want to install to /Applications, I use my adminbrew wrapper
    anyway.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,5 +1,6 @@
 export HOMEBREW_AUTO_UPDATE=1
 export HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-~/.brew}
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 HOMEBREWS_HOME="${HOMEBREWS_HOME:-$XDG_DATA_HOME/homebrews}"
 
 # Optional structure for a dual-homebrew setup for admin vs non-admin
