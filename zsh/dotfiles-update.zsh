24b1800cc7b784fbfde86be96175af7f275480ad
Christopher Larson <kergoth@gmail.com>
2020-04-06 09:01:09 -0700

    zsh: disable REPORTTIME for the dotfiles update
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/dotfiles-update.zsh
+++ zsh/dotfiles-update.zsh
@@ -1,7 +1,14 @@
+local oldtime=$REPORTTIME
+unset REPORTTIME
 if [[ -z $DOTFILES_NO_UPDATE ]]; then
     DOTFILES_STAMP=$XDG_CACHE_HOME/zsh/dotfiles-updated
     if [[ -e "$DOTFILESDIR/.git" ]] && [[ -n $DOTFILES_STAMP(#qN.mh+24) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
         echo >&2 "Checking for dotfiles updates (once daily)"
         "$DOTFILESDIR"/script/sync
     fi
+fi
+if [[ -n "$oldtime" ]]; then
+    REPORTTIME="$oldtime"
+else
+    unset REPORTTIME
 fi
