3238e282fe62e9423d7c406a5e31c0beb35b0969
Christopher Larson <kergoth@gmail.com>
2020-05-05 09:46:59 -0700

    dotfiles-update: use 14 hours, not 24
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/dotfiles-update.zsh
+++ zsh/dotfiles-update.zsh
@@ -2,7 +2,7 @@ local oldtime=$REPORTTIME
 unset REPORTTIME
 if [[ -z $DOTFILES_NO_UPDATE ]]; then
     DOTFILES_STAMP=$XDG_CACHE_HOME/zsh/dotfiles-updated
-    if [[ -e "$DOTFILESDIR/.git" ]] && [[ -n $DOTFILES_STAMP(#qN.mh+24) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
+    if [[ -e "$DOTFILESDIR/.git" ]] && [[ -n $DOTFILES_STAMP(#qN.mh+14) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
         echo >&2 "Checking for dotfiles updates (once daily)"
         "$DOTFILESDIR"/script/sync
     fi
