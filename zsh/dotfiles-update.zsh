7c98afbf3b66c4274b586d5c90853c7585cedbbe
Christopher Larson <kergoth@gmail.com>
2020-05-22 08:34:34 -0700

    Stop checking for vim updates daily
    Updating my vim plugins isn't exactly critical, the main purpose of the
    dotfiles update was to keep my configurations in sync, not externals, so
    update the vim plugins weekly instead.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/dotfiles-update.zsh
+++ zsh/dotfiles-update.zsh
@@ -4,7 +4,14 @@ if [[ -z $DOTFILES_NO_UPDATE ]]; then
     DOTFILES_STAMP=$XDG_CACHE_HOME/zsh/dotfiles-updated
     if [[ -e "$DOTFILESDIR/.git" ]] && [[ -n $DOTFILES_STAMP(#qN.mh+14) ]] || [[ ! -f $DOTFILES_STAMP ]]; then
         echo >&2 "Checking for dotfiles updates (once daily)"
-        "$DOTFILESDIR"/script/sync
+        "$DOTFILESDIR"/script/sync && touch "$DOTFILES_STAMP"
+    fi
+    VIM_STAMP=$XDG_CACHE_HOME/zsh/vim-updated
+    if [[ -e "$DOTFILESDIR/vim/.git" ]] && [[ -n $VIM_STAMP(#qN.mh+160) ]] || [[ ! -f $VIM_STAMP ]]; then
+        echo >&2 "Checking for vim updates (once weekly)"
+        if [ -e "$DOTFILESDIR"/vim/script/bootstrap ]; then
+            "$DOTFILESDIR"/vim/script/bootstrap && touch "$VIM_STAMP"
+        fi
     fi
 fi
 if [[ -n "$oldtime" ]]; then
