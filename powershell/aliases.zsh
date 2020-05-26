325fd65da8a67770f3da16e494463b612d26640c
Christopher Larson <kergoth@gmail.com>
2020-05-26 08:13:00 -0700

    powershell: for wsl, drop alias to the .exe versions
    When on WSL, having these precludes use of powershell core on Linux,
    which I may well want to use, rather than only the windows native
    versions.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- powershell/aliases.zsh
+++
@@ -1,8 +0,0 @@
-if [[ "$OSTYPE" = WSL ]]; then
-    alias pwsh=pwsh.exe
-    if (( $+commands[pwsh.exe] )); then
-        alias powershell=pwsh.exe
-    else
-        alias powershell=powershell.exe
-    fi
-fi
