f3e5cf791cf5d6bfa9fe15a4fdce6384c36bb9b2
Christopher Larson <kergoth@gmail.com>
2020-06-17 11:53:47 -0700

    Update externals
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/plugins/syntax-highlighting/highlighters/main/test-data/redirection-from-param.zsh
+++ zsh/plugins/syntax-highlighting/highlighters/main/test-data/redirection-from-param.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 # -------------------------------------------------------------------------------------------------
-# Copyright (c) 2019 zsh-syntax-highlighting contributors
+# Copyright (c) 2020 zsh-syntax-highlighting contributors
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted
@@ -28,14 +28,13 @@
 # vim: ft=zsh sw=2 ts=2 et
 # -------------------------------------------------------------------------------------------------
 
-BUFFER=$'a=( foo ; bar )'
+touch file
+local fn=$PWD/file
+
+BUFFER=$'<$fn cat'
 
 expected_region_highlight=(
-  '1 3 assign' # a=(
-  '3 3 reserved-word' # (
-  '5 7 default' # foo
-  '9 9 unknown-token' # ; (not commandseparator; see highlighter source code)
-  '11 13 default' # bar
-  '15 15 assign' # )
-  '15 15 reserved-word' # )
+  '1 1 redirection' # <
+  '2 4 path' # $fn
+  '6 8 command' # cat
 )
