3b84e9d86c425341a2731b9e41dfbb36b8bb0113
Christopher Larson <kergoth@gmail.com>
2020-06-25 09:09:17 -0700

    os-wsl: work with the npiperelay approach to openssh-ssh-agent
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- os-wsl/env.zsh
+++ os-wsl/env.zsh
@@ -15,8 +15,18 @@ case "$(uname -r)" in
         USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
 
         export BROWSER="cmd.exe /C START"
+
         if [[ -z "$SSH_AUTH_SOCK" ]] && [[ -n "$WSL_AUTH_SOCK" ]]; then
             export SSH_AUTH_SOCK=$WSL_AUTH_SOCK
+        else
+            NPIPERELAY="${NPIPERELAY:-$USERPROFILE/Apps/npiperelay.exe}"
+            if [[ -e "$NPIPERELAY" ]] && (( $+commands[socat] )); then
+                export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+                if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
+                    rm -f "$SSH_AUTH_SOCK"
+                    ( setsid socat "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+                fi
+            fi
         fi
         ;;
 esac
