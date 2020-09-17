d9ec89b0d032f645a7b0ffe469ca7ee4b75294c4
Christopher Larson <kergoth@gmail.com>
2020-09-17 10:24:34 -0700

    git: add ghcl/gist aliases using gh
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,3 +3,6 @@ alias gitnofancy='git -c "core.pager=diff-highlight | less --tabs=4 -RFX"'
 alias gitnopager='git -c "core.pager=cat"'
 
 alias gcl='git clone --recursive'
+
+alias ghcl='gh repo clone'
+alias gist='gh gist create'
