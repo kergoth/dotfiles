3a106e147fdccdd4d80703df3680deed66cdca34
Christopher Larson <kergoth@gmail.com>
2020-09-21 21:45:15 -0700

    git: add gncl alias
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,6 +3,7 @@ alias gitnofancy='git -c "core.pager=diff-highlight | less --tabs=4 -RFX"'
 alias gitnopager='git -c "core.pager=cat"'
 
 alias gcl='git clone --recursive'
+alias gncl='git clone'
 
 alias ghcl='gh repo clone'
 alias gist='gh gist create'
