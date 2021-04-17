a37ba3a70064c24385bbe77fa16d4c6608fb0e17
Christopher Larson <kergoth@gmail.com>
2021-04-16 19:50:19 -0700

    perl: remove, I never use it
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- perl/env.zsh
+++
@@ -1,9 +0,0 @@
-CPANM_LOCALDIR=${CPANM_LOCALDIR:-$XDG_DATA_HOME/cpanm}
-if [ -e "$CPANM_LOCALDIR/lib/perl5" ]; then
-    local perllocal_cache="$XDG_DATA_HOME/zsh/perllocal.zsh"
-    if [[ ! -e $perllocal_cache ]]; then
-        mkdir -p $XDG_DATA_HOME/perllocal
-        perl -I $CPANM_LOCALDIR/lib/perl5 -Mlocal::lib=$CPANM_LOCALDIR >$perllocal_cache
-    fi
-    source $perllocal_cache
-fi
