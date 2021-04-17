2f6548b0a79d7ae09a261d048ee744ab81ffe975
Christopher Larson <kergoth@gmail.com>
2021-04-16 20:42:06 -0700

    less: avoid the man func when using bat
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- less/less.zsh
+++ less/less.zsh
@@ -5,4 +5,18 @@ export LESS='-F -g -i -M -R -w -X -z-4'
 
 if (( $+commands[less] )); then
     export PAGER=less
+
+    if ! (( $+commands[bat] )); then
+	man () {
+            env \
+                LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+                LESS_TERMCAP_md=$(printf "\e[1;31m") \
+                LESS_TERMCAP_me=$(printf "\e[0m") \
+                LESS_TERMCAP_se=$(printf "\e[0m") \
+                LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+                LESS_TERMCAP_ue=$(printf "\e[0m") \
+                LESS_TERMCAP_us=$(printf "\e[1;32m") \
+                man "$@"
+        }
+    fi
 fi
