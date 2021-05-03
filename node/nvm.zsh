65a8416de09f33843d51bb9ad6a1c74b89b1bbb2
Christopher Larson <kergoth@gmail.com>
2021-05-03 10:43:11 -0700

    node: drop nvm, using asdf
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- node/nvm.zsh
+++
@@ -1,8 +0,0 @@
-if [[ -s "$NVM_DIR/nvm.sh" ]]; then
-    if [[ -n "$NODE_VERSION" ]]; then
-        . "$NVM_DIR/nvm.sh" --no-use
-        path=($NVM_DIR/versions/node/$NODE_VERSION/bin $path)
-    else
-        . "$NVM_DIR/nvm.sh"
-    fi
-fi
