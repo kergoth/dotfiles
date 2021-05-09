c8a6944dcc3b8cf99dc7f2f9d465a009b1c46202
Christopher Larson <kergoth@gmail.com>
2021-05-08 19:42:08 -0700

    keychain: move dir to xdg path
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- keychain/keychain.zsh
+++ keychain/keychain.zsh
@@ -1,3 +1,4 @@
 if [[ -z "$SSH_AUTH_SOCK" ]] && (( $+commands[keychain] )); then
     eval "$(keychain --eval)"
+    alias keychain='keychain --absolute --dir "${XDG_RUNTIME_DIR:-$XDG_DATA_HOME}"/keychain'
 fi
