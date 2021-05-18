5d510e66790dc9a963e3c12322d0ba427fbaae33
Christopher Larson <kergoth@gmail.com>
2021-05-18 08:47:23 -0700

    zsh: cache mcfly zsh init
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/final.zsh
+++ zsh/final.zsh
@@ -1,4 +1,11 @@
 if (( $+commands[mcfly] )); then
     MCFLY_PATH="$(command -v mcfly)"
-    eval "$(mcfly init zsh)"
+
+    mcfly_cache="$XDG_CACHE_HOME/mcfly/env.zsh"
+    if [[ ! -e $mcfly_cache ]]; then
+        mkdir -p $XDG_CACHE_HOME/mcfly
+        mcfly init zsh >$mcfly_cache || rm -f $mcfly_cache
+    fi
+    source $mcfly_cache
+    unset mcfly_cache
 fi
