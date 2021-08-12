3f7d3c45f69c4c69ebb724a81d5a79c72bf5589f
Christopher Larson <kergoth@gmail.com>
2021-08-11 19:34:49 -0700

    os-wsl: fix npiperelay
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- os-wsl/env.zsh
+++ os-wsl/env.zsh
@@ -2,6 +2,7 @@ case "$(uname -r)" in
     *-Microsoft)
         OSTYPE=WSL
         WSLVER=1
+        export USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
         if [[ -z "$SSH_AUTH_SOCK" ]] && [[ -n "$WSL_AUTH_SOCK" ]]; then
             export SSH_AUTH_SOCK=$WSL_AUTH_SOCK
         fi
@@ -9,12 +10,13 @@ case "$(uname -r)" in
     *-microsoft-*)
         OSTYPE=WSL
         WSLVER=2
+        export USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
         NPIPERELAY="${NPIPERELAY:-$USERPROFILE/Apps/npiperelay/npiperelay.exe}"
         if [[ -e "$NPIPERELAY" ]] && (( $+commands[socat] )); then
             export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
             if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
                 rm -f "$SSH_AUTH_SOCK"
-                ( setsid socat "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+                ( setsid socat "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork & )
             fi
         fi
         ;;
@@ -33,6 +35,5 @@ if [[ "$OSTYPE" = "WSL" ]]; then
             fi
         fi
     fi
-    export USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
     export BROWSER="cmd.exe /C START"
 fi
