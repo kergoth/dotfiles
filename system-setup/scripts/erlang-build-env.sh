e91b25acbdf951b79392baa472f4534a5a0d89ca
Christopher Larson <kergoth@gmail.com>
2021-09-26 21:18:34 -0700

    system-setup: remove, handling this elsewhere
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- system-setup/scripts/erlang-build-env.sh
+++
@@ -1,9 +0,0 @@
-export KERL_CONFIGURE_OPTIONS="--without-javac"
-
-case "${OSTYPE:-}" in
-    darwin*)
-        if command -v brew >/dev/null 2>&1; then
-            KERL_CONFIGURE_OPTIONS="$KERL_CONFIGURE_OPTIONS --with-ssl=$(brew --prefix openssl)"
-        fi
-        ;;
-esac
--- system-setup/scripts/nodejs-build-env.sh
+++
@@ -1,1 +0,0 @@
-bash "$ASDF_DATA_DIR/plugins/nodejs/bin/import-release-team-keyring" >/dev/null 2>&1
