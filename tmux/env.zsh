a24f59be612341175a1f6325bda78d0daec25860
Christopher Larson <kergoth@gmail.com>
2021-10-07 08:44:20 -0700

    tmux: correct env.zsh
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- tmux/env.zsh
+++ tmux/env.zsh
@@ -1,2 +1,6 @@
-export TMUX_TMPDIR=$XDG_RUNTIME_DIR
-export TERM_PROGRAM
+if [[ -n $XDG_RUNTIME_DIR ]]; then
+    export TMUX_TMPDIR=$XDG_RUNTIME_DIR
+fi
+if [[ -n $TERM_PROGRAM ]]; then
+    export TERM_PROGRAM
+fi
