21e9f59271d1883da87e11c9944812933666171b
Christopher Larson <kergoth@gmail.com>
2022-04-08 08:22:54 -0700

    zsh: skip auto_update_env unless we're within tmux/att
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/auto_update_env.zsh
+++ zsh/auto_update_env.zsh
@@ -1,8 +1,10 @@
-for cmd in repo mr ssh scp code e dbitbake; do
-eval "
-    function $cmd {
-        update_env
-        command $cmd "\$@"
-    }
-"
-done
+if [[ -n "$TMUX" ]] || [[ -n "$ATT_SESSION" ]]; then
+    for cmd in repo mr ssh scp code e dbitbake; do
+    eval "
+        function $cmd {
+            update_env
+            command $cmd "\$@"
+        }
+    "
+    done
+fi
