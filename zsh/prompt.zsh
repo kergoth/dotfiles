a9d96b10079cfd097124fb3228893c9f8aafeb0b
Christopher Larson <kergoth@gmail.com>
2022-04-08 08:38:23 -0700

    zsh: skip prompt setup with warp terminal
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,10 @@
-. $ZSH/themes/powerlevel10k/powerlevel10k.zsh-theme
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+    . $ZSH/themes/powerlevel10k/powerlevel10k.zsh-theme
 
-if [[ $OSTYPE = WSL ]] && [[ $WSL_IS_ADMIN = 1 ]]; then
-    add_wsl_admin() {
-        RPROMPT="$RPROMPT [wsladmin]"
-    }
-    add-zsh-hook precmd add_wsl_admin
+    if [[ $OSTYPE = WSL ]] && [[ $WSL_IS_ADMIN = 1 ]]; then
+        add_wsl_admin() {
+            RPROMPT="$RPROMPT [wsladmin]"
+        }
+        add-zsh-hook precmd add_wsl_admin
+    fi
 fi
