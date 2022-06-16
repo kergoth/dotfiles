3f4c163bd94a0899e17ef64c8140c2eb3e262182
Christopher Larson <chris_larson@mentor.com>
2022-06-16 20:23:44 +0500

    bitbake: bbs -> bbg, add bbrg
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- bitbake/bitbake.zsh
+++ bitbake/bitbake.zsh
@@ -4,16 +4,19 @@ alias bb-getvar=bitbake-getvar
 alias bbgetvar=bitbake-getvar
 alias bbenvvar=bitbake-getvar
 
-if (( $+commands[pt] )); then
+if (( $+commands[rg] )); then
+    alias bbrg="rg -t bitbake"
+    alias bbg=bbrg
+elif (( $+commands[pt] )); then
     alias bbag="pt -G '\.(bb|bbappend|inc|conf)$'"
-    alias bbs=bbag
+    alias bbg=bbag
 elif (( $+commands[ag] )); then
     alias bbag="ag -G '\.(bb|bbappend|inc|conf)$'"
-    alias bbs=bbag
+    alias bbg=bbag
 elif (( $+commands[ack] )); then
     alias bback='ack --type=bitbake'
     alias bbag=bback
-    alias bbs=bbag
+    alias bbg=bbag
 fi
 alias bbfd="fd -t f -e bb -e inc -e conf -e bbclass -e bbappend"
 alias bbfdf="bbfd ''"
