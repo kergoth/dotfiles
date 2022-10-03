d0e30e117b296cd8e54891d6e258dec4bf723561
Christopher Larson <kergoth@gmail.com>
2022-10-02 21:16:58 -0700

    nix: add aliases
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ nix/alias.zsh
@@ -0,0 +1,7 @@
+alias nixhome="home-manager switch"
+alias nixgc="nix-collect-garbage -d"
+alias nixq="nix-env -qaP"
+alias nixup="nix-env -u"
+alias nixupgrade="nix-channel --update && nix-env -iA nixpkgs.nix"
+# alias nixupgrade="sudo -i sh -c 'nix-channel --update && nix-env -iA nixpkgs.nix && launchctl remove org.nixos.nix-daemon && launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist'"
+alias nixcfg="e ~/.config/nixpkgs/home.nix"
