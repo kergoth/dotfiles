9b0aee01d4591ff0fcd9975b8485cd3ab9078cc3
Christopher Larson <chris_larson@mentor.com>
2022-10-03 22:53:11 +0500

    direnv: handle install via nix in home
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- direnv/early.zsh
+++ direnv/early.zsh
@@ -1,3 +1,7 @@
 if (( ${+commands[direnv]} )); then
-    emulate zsh -c "$(direnv export zsh)"
+    if [ "$home_nix" = 1 ] && [ $commands[direnv] = $HOME/.nix/shims/direnv ]; then
+        emulate zsh -c "$(nixrun direnv export zsh)"
+    else
+        emulate zsh -c "$(direnv export zsh)"
+    fi
 fi
--- direnv/hook.zsh
+++ direnv/hook.zsh
@@ -1,3 +1,7 @@
 if (( ${+commands[direnv]} )); then
-    emulate zsh -c "$(direnv hook zsh)"
+    if [ "$home_nix" = 1 ] && [ $commands[direnv] = $HOME/.nix/shims/direnv ]; then
+        emulate zsh -c "$(nixrun direnv hook zsh | sed -e "s#\"/nix.*/direnv\"#nixrun direnv#g")"
+    else
+        emulate zsh -c "$(direnv hook zsh)"
+    fi
 fi
