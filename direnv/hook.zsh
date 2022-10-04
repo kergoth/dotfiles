ec04c470c9ea979663add52af40c514184e334d3
Christopher Larson <chris_larson@mentor.com>
2022-10-05 00:37:31 +0500

    direnv: simplify how we run under home nix
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- direnv/hook.zsh
+++ direnv/hook.zsh
@@ -1,6 +1,6 @@
 if (( ${+commands[direnv]} )); then
     if [ "$home_nix" = 1 ] && [ $commands[direnv] = $HOME/.nix/shims/direnv ]; then
-        emulate zsh -c "$(nixrun direnv hook zsh | sed -e "s#\"/nix.*/direnv\"#nixrun direnv#g")"
+        emulate zsh -c "$(nixrun direnv hook zsh | sed -e "s#\"/nix#nixrun \"/nix#g")"
     else
         emulate zsh -c "$(direnv hook zsh)"
     fi
