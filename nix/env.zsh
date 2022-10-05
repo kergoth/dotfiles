10822bf89312228cdacfc31b6efd7cb7019e46a1
Christopher Larson <kergoth@gmail.com>
2022-10-05 10:08:47 -0700

    nix: set LOCALE_ARCHIVE
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- nix/env.zsh
+++ nix/env.zsh
@@ -1,1 +1,5 @@
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+
+if [ -e /usr/lib/locale/locale-archive ]; then
+    export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+fi
