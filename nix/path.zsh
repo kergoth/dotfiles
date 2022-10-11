307ce6410bff72444bccea346d759935b9a04ddf
Christopher Larson <kergoth@gmail.com>
2022-10-11 10:41:30 -0700

    nix: let .local/bin override nix binaries
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- nix/path.zsh
+++ nix/path.zsh
@@ -15,5 +15,5 @@ for f in etc/profile.d/nix-daemon.sh etc/profile.d/nix.sh etc/profile.d/hm-sessi
     done
 done
 
-path=(~/.nix/shims $path)
+path=(~/.local/bin ~/.nix/shims $path)
 manpath=($HOME/.nix-profile/share/man $manpath)
