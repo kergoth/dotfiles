8b2b1af4ec933529a9decb5a572bbb6e511b849a
Christopher Larson <kergoth@gmail.com>
2022-11-13 21:29:27 -0700

    Cache xcrun --show-sdk-path output
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- os-macos/cflags.zsh
+++ os-macos/cflags.zsh
@@ -1,3 +1,6 @@
 if [[ $OSTYPE =~ darwin ]]; then
-    export CFLAGS="$CFLAGS -I$(xcrun --show-sdk-path)/usr/include"
+    if ! [[ -e $XDG_CACHE_HOME/zsh/xcrun ]]; then
+        xcrun --show-sdk-path >$XDG_CACHE_HOME/zsh/xcrun
+    fi
+    export CFLAGS="$CFLAGS -I$(<$XDG_CACHE_HOME/zsh/xcrun)/usr/include"
 fi
