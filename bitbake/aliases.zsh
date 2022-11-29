8196bd1c8689990813ccc2cee4a4d136666b65fd
Christopher Larson <chris_larson@mentor.com>
2022-11-29 19:52:27 +0000

    bitbake: rework docker wrapper script bits
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

---
+++ bitbake/aliases.zsh
@@ -0,0 +1,42 @@
+get-bbvar () {
+    local var="$1"
+    shift
+    if [ $# -gt 0 ]; then
+        local recipe="$1"
+        shift
+        set -- -r "$recipe" "$@"
+    fi
+    (eval $(bb show -b "$@" "$var" | grep -v Parsing); eval "printf '%s' \"\$$var\"";)
+}
+
+cd-workdir () {
+    cd "$(get-bbvar WORKDIR "$1" | tr -d '\r\n')"
+}
+
+cd-srcdir () {
+    cd "$(get-bbvar S "$1" | tr -d '\r\n')"
+}
+
+cd-objdir () {
+    cd "$(get-bbvar B "$1" | tr -d '\r\n')"
+}
+
+bbrebuild () {
+    bitbake -c clean "$@" && bitbake -C configure -k "$@"
+}
+
+bbrebake () {
+    bitbake -c clean "$@" && bitbake -k "$@"
+}
+
+alias bblayers=bitbake-layers
+
+alias bb="oe-docker-run bb"
+alias bitbake-diffsigs="oe-docker-run bitbake-diffsigs"
+alias bitbake-dumpsig="oe-docker-run bitbake-dumpsig"
+alias bitbake-getvar="oe-docker-run bitbake-getvar"
+alias bitbake-getvars="oe-docker-run bitbake-getvars"
+alias bitbake-layers="oe-docker-run bitbake-layers"
+alias bitbake="oe-docker-run bitbake"
+alias devtool="oe-docker-run devtool"
+alias recipetool="oe-docker-run recipetool"
