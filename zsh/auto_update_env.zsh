985277b6fab1697d69272c2d84439e3d9c5d1d43
Christopher Larson <chris_larson@mentor.com>
2023-04-18 21:43:46 +0500

    zsh: also auto update env for wp/workspace-picker
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- zsh/auto_update_env.zsh
+++ zsh/auto_update_env.zsh
@@ -1,5 +1,5 @@
 if [[ -n "$TMUX" ]] || [[ -n "$ATT_SESSION" ]]; then
-    for cmd in repo mr ssh scp code e dbitbake; do
+    for cmd in repo mr ssh scp code e dbitbake wp workspace-picker; do
     eval "
         function $cmd {
             update_env
