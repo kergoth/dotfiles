619af073b5263eddc3388a56c274e15950b639ec
Christopher Larson <chris_larson@mentor.com>
2023-05-12 12:39:15 -0500

    ssh: fix agent forwarding with tmux+vscode
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

---
+++ ssh/env.zsh
@@ -0,0 +1,4 @@
+# Fix SSH auth socket location so agent forwarding works with tmux and VS Code
+if [ -n "$SSH_AUTH_SOCK" ] && [ -e "$HOME/.ssh/auth_sock" ]; then
+    export SSH_AUTH_SOCK="$HOME/.ssh/auth_sock"
+fi
