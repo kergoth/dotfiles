dcbaffee5b3e061087f2f2502b670234b34f0c3f
Christopher Larson <chris_larson@mentor.com>
2023-05-12 12:36:56 -0500

    nix: also point LOCALE_ARCHIVE to nix version
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- nix/env.zsh
+++ nix/env.zsh
@@ -1,5 +1,7 @@
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 
-if [ -e /usr/lib/locale/locale-archive ]; then
+if [ -e ~/.nix-profile/lib/locale/locale-archive ]; then
+    export LOCALE_ARCHIVE="$HOME/.nix-profile/lib/locale/locale-archive"
+elif [ -e /usr/lib/locale/locale-archive ]; then
     export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
 fi
