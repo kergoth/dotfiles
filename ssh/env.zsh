770b0786cf8167a2ea9453c5a3071f95b5ce4621
Christopher Larson <chris_larson@mentor.com>
2023-10-05 06:25:47 +0500

    ssh: avoid auth sock link recursion
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- ssh/env.zsh
+++ ssh/env.zsh
@@ -1,4 +1,4 @@
 # Fix SSH auth socket location so agent forwarding works with tmux and VS Code
-if [ -n "$SSH_AUTH_SOCK" ] && [ -e "$HOME/.ssh/auth_sock" ]; then
+if [ -n "$SSH_AUTH_SOCK" ] && [ -e "$HOME/.ssh/auth_sock" ] && [ "$SSH_AUTH_SOCK" != "$HOME/.ssh/auth_sock" ]; then
     export SSH_AUTH_SOCK="$HOME/.ssh/auth_sock"
 fi
