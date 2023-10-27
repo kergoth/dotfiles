5972a176bf32ca1ade532773aa79dca4a8e26d2d
Christopher Larson <kergoth@gmail.com>
2023-10-27 08:45:13 -0700

    kitty: attempt to fix integration
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- kitty/kitty.zsh
+++ kitty/kitty.zsh
@@ -1,11 +1,18 @@
-if test -n "$KITTY_INSTALLATION_DIR"; then
-    export KITTY_SHELL_INTEGRATION="enabled"
-    autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-    kitty-integration
-    unfunction kitty-integration
-
-    if (( $+commands[kitten] )); then
-        alias ssh="kitten ssh"
+if [[ "$TERM" = "xterm-kitty" ]] || [[ -n "$KITTY_WINDOW_ID" ]]; then
+    # if [[ -n "$XDG_DATA_HOME/kitty-ssh-kitten/kitty/bin" ]]; then
+    if [[ -z "$KITTY_INSTALLATION_DIR" ]] && [[ -n "$XDG_DATA_HOME/kitty-ssh-kitten" ]]; then
+        export KITTY_INSTALLATION_DIR="$XDG_DATA_HOME/kitty-ssh-kitten"
     fi
-fi
+    if [[ -n "$XDG_DATA_HOME/kitty-ssh-kitten/kitty/bin" ]]; then
+        path=($path "$XDG_DATA_HOME/kitty-ssh-kitten/kitty/bin")
+    fi
 
+    if [[ -n "$KITTY_INSTALLATION_DIR" ]]; then
+        export KITTY_SHELL_INTEGRATION="enabled"
+        autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+        kitty-integration
+        unfunction kitty-integration
+    fi
+
+    alias ssh="kitty +kitten ssh"
+fi
