605080fc6d67149428aa1b694731d59d43e10363
Christopher Larson <chris.larson@siemens.com>
2023-12-20 16:12:49 -0700

    Simplify the wsl zsh setup
    Signed-off-by: Christopher Larson <chris.larson@siemens.com>

--- home/dot_config/zsh/dot_zshrc.d/wsl.zsh
+++ home/dot_config/zsh/dot_zshrc.d/wsl.zsh
@@ -1,56 +1,35 @@
 if [[ ! -e /.dockerenv ]]; then
     case "$(uname -r)" in
-        *-Microsoft)
-            OSTYPE=WSL
-            WSLVER=1
-            export USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
-            if [[ -z "$SSH_AUTH_SOCK" ]] && [[ -n "$WSL_AUTH_SOCK" ]]; then
-                export SSH_AUTH_SOCK=$WSL_AUTH_SOCK
-            fi
-            ;;
         *-microsoft-*)
             OSTYPE=WSL
-            WSLVER=2
-            export USERPROFILE="${USERPROFILE:-$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")}"
-            if [[ -z "${NPIPERELAY:-}" ]]; then
-                for i in "$USERPROFILE/Apps/npiperelay/npiperelay.exe" "$USERPROFILE/scoop/apps/npiperelay/current/npiperelay.exe"; do
-                    if [[ -e "$i" ]]; then
-                        NPIPERELAY="$i"
-                        break
-                    fi
-                done
-            fi
-            if [[ -e "${NPIPERELAY:-}" ]]; then
-                if (( $+commands[socat] )); then
-                    export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-                    if ! ss -a | grep -q "$SSH_AUTH_SOCK" || ! pgrep -f socat &>/dev/null; then
-                        rm -f "$SSH_AUTH_SOCK"
-                        ( setsid socat "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork & )
-                    fi
-                else
-                    echo >&2 "Warning: socat is not installed, unable to use npiperelay for ssh auth sock"
-                fi
-            fi
             ;;
     esac
 fi
 
 if [[ "$OSTYPE" = "WSL" ]]; then
-    OSTYPE=WSL
-    if [[ -z "$WslDisks" ]]; then
-        export WslDisks=/mnt
-        if [[ -e /etc/wsl.conf ]]; then
-            WslDisks="$(sed -n -e 's/^root = //p' /etc/wsl.conf)"
-            if [[ -n $WslDisks ]]; then
-                export WslDisks="${WslDisks%/}"
-            else
-                WslDisks=/mnt
-            fi
+    export BROWSER="cmd.exe /C START"
+    if [[ -z "$USERPROFILE" ]]; then
+        if (( $+commands[cmd.exe] )); then
+            export USERPROFILE="$(wslpath "$(cmd.exe /D /C 'SET /P <NUL=%USERPROFILE%' 2>/dev/null)")"
+        else
+            echo >&2 "Error: cmd.exe not available, and USERPROFILE is not set"
         fi
     fi
-    export BROWSER="cmd.exe /C START"
-fi
 
-if [[ -o interactive ]] && [[ $OSTYPE = WSL ]] && [[ $PWD = $USERPROFILE ]]; then
-    cd
+    path=("$USERPROFILE/Apps/npiperelay" $path)
+    if (( $+commands[npiperelay.exe] )); then
+        if (( $+commands[socat] )); then
+            export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+            if ! ss -a | grep -q "$SSH_AUTH_SOCK" || ! pgrep -f socat &>/dev/null; then
+                rm -f "$SSH_AUTH_SOCK"
+                ( setsid socat "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork & )
+            fi
+        else
+            echo >&2 "Warning: socat is not installed, unable to use npiperelay to connect to Windows SSH agent."
+        fi
+    fi
+
+    if [[ -o interactive ]] && [[ $PWD = $USERPROFILE ]]; then
+        cd
+    fi
 fi
