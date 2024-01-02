5b1394526493e8e4a6c813e7523930b711a4f07e
Christopher Larson <kergoth@gmail.com>
2024-01-02 08:36:06 -0700

    Avoid checking if vim/script/update is present
    Chezmoi fetches this, it should be present. If I want to handle its
    absence, for example, due to a network issue, I can use a template in
    chezmoiignore to avoid running these scripts at all.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- home/.chezmoiscripts/run_onchange_after_vim.ps1
+++ home/.chezmoiscripts/run_onchange_after_vim.ps1
@@ -1,4 +1,1 @@
-$update = Join-Path $env:USERPROFILE .config\vim\script\update.ps1
-if (Test-Path $update) {
-    $update | Invoke-Expression
-}
+Join-Path $env:USERPROFILE .config\vim\script\update.ps1 | Invoke-Expression
