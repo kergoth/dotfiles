02e4d44016e3b90e70794ad9498a7a1f80477227
Christopher Larson <chris.larson@siemens.com>
2024-01-04 10:40:58 -0700

    Skip loading my profile when elevating powershell to admin
    Signed-off-by: Christopher Larson <chris.larson@siemens.com>

--- scripts/setup-system-windows.ps1
+++ scripts/setup-system-windows.ps1
@@ -1,7 +1,7 @@
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        $CommandLine = "-NoProfile -NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
         Start-Process -Wait -FilePath (Get-Process -pid $pid).Path -Verb Runas -ArgumentList $CommandLine
         Exit
     }
