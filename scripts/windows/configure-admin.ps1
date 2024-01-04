a6ea269c54aa7da597a9669a9c8dfe71938dcd1b
Christopher Larson <chris.larson@siemens.com>
2024-01-04 10:44:22 -0700

    Be clear about which changes require Sophia
    Signed-off-by: Christopher Larson <chris.larson@siemens.com>

--- scripts/windows/configure-admin.ps1
+++ scripts/windows/configure-admin.ps1
@@ -9,66 +9,16 @@ function Invoke-Sophia {
     catch {
     }
 
-    # Enable app sideloading
-    New-ItemProperty -Path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps -Type DWord -Value 1 -Force
-
-    # Enable windows developer mode
-    New-ItemProperty -Path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Type DWord -Value 1 -Force
-
-    # Small icons in taskbar
-    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "TaskbarSmallIcons" -Type DWord -Value 1
+    # Uninstall OneDrive
+    OneDrive -Uninstall
+    Remove-PossiblyMissingItem -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force
 
     # Associate *.txt to Notepad++
     if (Test-Path "$env:ProgramFiles\Notepad++\notepad++.exe") {
         Set-Association -ProgramPath "%ProgramFiles%\Notepad++\notepad++.exe" -Extension .txt -Icon "%ProgramFiles%\Notepad++\notepad++.exe,0"
     }
 
-    # Uninstall OneDrive
-    OneDrive -Uninstall
-    Remove-PossiblyMissingItem -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force
-
-    # Disable Scheduled Tasks
-    [string[]]$CheckedScheduledTasks = @(
-        # Collects program telemetry information if opted-in to the Microsoft Customer Experience Improvement Program
-        "ProgramDataUpdater",
-
-        # This task collects and uploads autochk SQM data if opted-in to the Microsoft Customer Experience Improvement Program
-        "Proxy",
-
-        # If the user has consented to participate in the Windows Customer Experience Improvement Program, this job collects and sends usage data to Microsoft
-        "Consolidator",
-
-        # The USB CEIP (Customer Experience Improvement Program) task collects Universal Serial Bus related statistics and information about your machine and sends it to the Windows Device Connectivity engineering group at Microsoft
-        "UsbCeip",
-
-        # The Windows Disk Diagnostic reports general disk and system information to Microsoft for users participating in the Customer Experience Program
-        "Microsoft-Windows-DiskDiagnosticDataCollector"
-    )
-
-    $Tasks = Get-ScheduledTask | Where-Object -FilterScript { ($_.State -eq "Ready") -and ($_.TaskName -in $CheckedScheduledTasks) }
-    $Tasks | Disable-ScheduledTask
-
-    # Sophia -Functions "Set-Association -ProgramPath `"C:\Program Files\Google\Chrome\Application\chrome.exe`" -Extension .html"
-    # Sophia -Functions "Set-Association -ProgramPath `"C:\Program Files\Google\Chrome\Application\chrome.exe`" -Extension .htm"
-    # Review: PinToStart
-    # Review: UninstallUWPApps
-    # Review: BackgroundUWPApps -Disable
-    # Review: CheckUWPAppsUpdates
-    # Review: GPUScheduling -Enable
-    # Are these needed with storage sense enabled? TempTask -Register, SoftwareDistributionTask -Register, CleanupTask -Register
-
     DiagTrackService -Disable
-
-    # Minimize diagnostic data collection
-    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection -Name AllowTelemetry -PropertyType DWord -Value 1 -Force
-    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection -Name MaxTelemetryAllowed -PropertyType DWord -Value 1 -Force
-    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Diagnostics\DiagTrack -Name ShowedToastAtLevel -PropertyType DWord -Value 1 -Force
-
-    # Disable Error Reporting
-    Get-ScheduledTask -TaskName QueueReporting | Disable-ScheduledTask
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name Disabled -PropertyType DWord -Value 1 -Force
-    Get-Service -Name WerSvc | Stop-Service -Force
-    Get-Service -Name WerSvc | Set-Service -StartupType Disabled
 
     FeedbackFrequency -Never
     SigninInfo -Disable
@@ -130,36 +80,6 @@ function Invoke-Sophia {
     XboxGameBar -Disable
     XboxGameTips -Disable
 
-    # Enable Network Protection
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
-        New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender" -ItemType Directory -Force
-    }
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard")) {
-        New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard" -ItemType Directory -Force
-    }
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection")) {
-        New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection" -ItemType Directory -Force
-    }
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection" -Name EnableNetworkProtection -PropertyType DWord -Value 1 -Force
-
-    # Enable Potentially Unwanted Applications Protection
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
-        New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender" -ItemType Directory -Force
-    }
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine")) {
-        New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -ItemType Directory -Force
-    }
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -Name MpEnablePus -PropertyType DWord -Value 1 -Force
-
-    # Enable Defender Sandboxing
-    setx /M MP_FORCE_USE_SANDBOX 1
-
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health" -ItemType Directory -Force
-    }
-    if (-not (Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State" -ItemType Directory -Force
-    }
     DismissMSAccount
     DismissSmartScreenFilter
 
@@ -173,14 +93,88 @@ function Invoke-Sophia {
 
 . $PSScriptRoot\..\common.ps1
 
-# SSH
-Write-Output "Enabling SSH agent"
+# Start SSH agent and set the service to be started automatically
+$service = Get-Service -Name ssh-agent -ErrorAction SilentlyContinue
+if ($service) {
+    if ($service.StartupType -ne [Microsoft.PowerShell.Commands.ServiceStartupType]::Automatic) {
+        $service | Set-Service -StartupType Automatic
+    }
+    if ($service.Status -ne [System.ServiceProcess.ServiceControllerStatus]::Running) {
+        $service | Start-Service 
+    }
+}
 
-# Set the ssh agent service to be started automatically
-Get-Service -Name ssh-agent | Set-Service -StartupType Automatic
+# Enable Network Protection
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender" -ItemType Directory -Force
+}
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard")) {
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard" -ItemType Directory -Force
+}
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection")) {
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection" -ItemType Directory -Force
+}
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection" -Name EnableNetworkProtection -PropertyType DWord -Value 1 -Force
 
-# Now start the service
-Start-Service ssh-agent
+# Enable Potentially Unwanted Applications Protection
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender" -ItemType Directory -Force
+}
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine")) {
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -ItemType Directory -Force
+}
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -Name MpEnablePus -PropertyType DWord -Value 1 -Force
+
+# Enable Defender Sandboxing
+setx /M MP_FORCE_USE_SANDBOX 1
+
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health")) {
+    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health" -ItemType Directory -Force
+}
+if (-not (Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State")) {
+    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State" -ItemType Directory -Force
+}
+
+# Enable app sideloading
+New-ItemProperty -Path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps -Type DWord -Value 1 -Force
+
+# Enable windows developer mode
+New-ItemProperty -Path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Type DWord -Value 1 -Force
+
+# Small icons in taskbar
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "TaskbarSmallIcons" -Type DWord -Value 1
+
+# Minimize diagnostic data collection
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection -Name AllowTelemetry -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection -Name MaxTelemetryAllowed -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Diagnostics\DiagTrack -Name ShowedToastAtLevel -PropertyType DWord -Value 1 -Force
+
+# Disable Scheduled Tasks
+[string[]]$CheckedScheduledTasks = @(
+    # Collects program telemetry information if opted-in to the Microsoft Customer Experience Improvement Program
+    "ProgramDataUpdater",
+
+    # This task collects and uploads autochk SQM data if opted-in to the Microsoft Customer Experience Improvement Program
+    "Proxy",
+
+    # If the user has consented to participate in the Windows Customer Experience Improvement Program, this job collects and sends usage data to Microsoft
+    "Consolidator",
+
+    # The USB CEIP (Customer Experience Improvement Program) task collects Universal Serial Bus related statistics and information about your machine and sends it to the Windows Device Connectivity engineering group at Microsoft
+    "UsbCeip",
+
+    # The Windows Disk Diagnostic reports general disk and system information to Microsoft for users participating in the Customer Experience Program
+    "Microsoft-Windows-DiskDiagnosticDataCollector"
+)
+
+$Tasks = Get-ScheduledTask | Where-Object -FilterScript { ($_.State -eq "Ready") -and ($_.TaskName -in $CheckedScheduledTasks) }
+$Tasks | Disable-ScheduledTask
+
+# Disable Error Reporting
+Get-ScheduledTask -TaskName QueueReporting | Disable-ScheduledTask
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name Disabled -PropertyType DWord -Value 1 -Force
+Get-Service -Name WerSvc | Stop-Service -Force
+Get-Service -Name WerSvc | Set-Service -StartupType Disabled
 
 # System configuration via Sophia
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
