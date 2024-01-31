fe933f70c9a6a54ad2a341889751b7fef8bedbe9
Christopher Larson <kergoth@gmail.com>
2024-01-30 22:24:25 -0700

    Only install Microsoft.WinGet.Client on Windows
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- scripts/install-pwsh-modules.ps1
+++ scripts/install-pwsh-modules.ps1
@@ -2,12 +2,14 @@
 
 . $PSScriptRoot\common.ps1
 
-$ProgressPreference = 'SilentlyContinue' # Suppress progress bar (speed up downloading, especially on PowerShell 5)
 $ConfirmPreference = 'None' # Suppress confirmation prompts
 
 Write-Verbose "Installing PowerShell modules"
 Install-ModuleIfNotInstalled DirColors
-Install-ModuleIfNotInstalled Microsoft.WinGet.Client
 Install-ModuleIfNotInstalled posh-alias
 Install-ModuleIfNotInstalled PSFzf
 Install-ModuleIfNotInstalled Recycle
+
+if ($IsWindows) {
+    Install-ModuleIfNotInstalled Microsoft.WinGet.Client
+}
