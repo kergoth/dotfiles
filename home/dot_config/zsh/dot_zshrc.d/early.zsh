7a798ef7001e08f6a33ddb00bf333878f69ef132
Christopher Larson <chris.larson@siemens.com>
2024-02-26 08:42:30 -0700

    Create /run/user/ path on WSL if needed
    Signed-off-by: Christopher Larson <chris.larson@siemens.com>

--- home/dot_config/zsh/dot_zshrc.d/early.zsh
+++ home/dot_config/zsh/dot_zshrc.d/early.zsh
@@ -6,6 +6,15 @@ if [[ ! -e /.dockerenv ]]; then
     esac
 fi
 
+if [[ "$OSTYPE" = WSL ]]; then
+    UID=${UID:-$(id -u)}
+    if ! [[ -d /run/user/"$UID" ]]; then
+        echo >&2 "Warning: /run/user/$UID does not exist. Creating using sudo."
+        sudo mkdir -p /run/user/"$UID" && \
+            sudo chown -R "$UID" /run/user/"$UID"
+    fi
+fi
+
 if (( ${+commands[direnv]} )); then
     if [ "$home_nix" = 1 ] && [ $commands[direnv] = $HOME/.nix/shims/direnv ]; then
         emulate zsh -c "$(nixrun direnv export zsh)"
