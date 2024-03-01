a326bdf1699b5d6f4d4dcfcfe83b989709705591
Christopher Larson <kergoth@gmail.com>
2024-03-01 11:15:46 -0700

    Fix check for existence of broot
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/broot.zsh
+++ home/dot_config/zsh/dot_zshrc.d/broot.zsh
@@ -1,4 +1,4 @@
-if (( $commands[broot] )); then
+if (( $+commands[broot] )); then
     if ! [[ -e "$XDG_CACHE_HOME/zsh/functions/br" ]] || [[ $commands[broot] -nt "$XDG_CACHE_HOME/zsh/functions/br" ]]; then
         broot --print-shell-function zsh >"$XDG_CACHE_HOME/zsh/functions/br"
         broot --set-install-state installed
