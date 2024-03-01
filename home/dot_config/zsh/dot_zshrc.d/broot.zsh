0771ad30a64bc7c57c26823dd6ae2121bda4b1df
Christopher Larson <kergoth@gmail.com>
2024-03-01 11:15:58 -0700

    Silence output from broot at startup
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/broot.zsh
+++ home/dot_config/zsh/dot_zshrc.d/broot.zsh
@@ -1,6 +1,6 @@
 if (( $+commands[broot] )); then
     if ! [[ -e "$XDG_CACHE_HOME/zsh/functions/br" ]] || [[ $commands[broot] -nt "$XDG_CACHE_HOME/zsh/functions/br" ]]; then
         broot --print-shell-function zsh >"$XDG_CACHE_HOME/zsh/functions/br"
-        broot --set-install-state installed
+        broot --set-install-state installed &>/dev/null
     fi
 fi
