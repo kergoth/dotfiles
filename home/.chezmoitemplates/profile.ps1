f64162c299be43bbefe87e8f6af1de6327df9f64
Christopher Larson <chris.larson@siemens.com>
2024-05-10 10:32:20 -0700

    Split the editor/visual value for powershell
    Signed-off-by: Christopher Larson <chris.larson@siemens.com>

--- home/.chezmoitemplates/profile.ps1
+++ home/.chezmoitemplates/profile.ps1
@@ -350,6 +350,115 @@ if (Get-Command nvim -ErrorAction SilentlyContinue) {
 }
 Add-Alias vi vim
 
+function Split-CommandLine {
+    <#
+    .Synopsis
+        Parse command-line arguments using Win32 API CommandLineToArgvW function.
+
+    .Link
+        http://edgylogic.com/blog/powershell-and-external-commands-done-right/
+
+    .Description
+        This is the Cmdlet version of the code from the article http://edgylogic.com/blog/powershell-and-external-commands-done-right.
+        It can parse command-line arguments using Win32 API function CommandLineToArgvW .
+
+    .Parameter CommandLine
+        This parameter is optional.
+
+        A string representing the command-line to parse. If not specified, the command-line of the current PowerShell host is used.
+
+    .Example
+        Split-CommandLine
+
+            Description
+            -----------
+            Get the command-line of the current PowerShell host, parse it and return arguments.
+
+    .Example
+        Split-CommandLine -CommandLine '"c:\windows\notepad.exe" test.txt'
+
+            Description
+            -----------
+            Parse user-specified command-line and return arguments.
+
+    .Example
+        '"c:\windows\notepad.exe" test.txt',  '%SystemRoot%\system32\svchost.exe -k LocalServiceNetworkRestricted' | Split-CommandLine
+
+            Description
+            -----------
+            Parse user-specified command-line from pipeline input and return arguments.
+
+    .Example
+        Get-WmiObject Win32_Process -Filter "Name='notepad.exe'" | Split-CommandLine
+
+            Description
+            -----------
+            Parse user-specified command-line from property name of the pipeline object and return arguments.
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CommandLine
+    )
+
+    Begin {
+        $Kernel32Definition = @'
+            [DllImport("kernel32")]
+            public static extern IntPtr GetCommandLineW();
+            [DllImport("kernel32")]
+            public static extern IntPtr LocalFree(IntPtr hMem);
+'@
+        # $Kernel32 = Add-Type -MemberDefinition $Kernel32Definition -Name 'Kernel32' -Namespace 'Win32' -PassThru
+        if (-Not $Kernel32::GetCommandLineW) {
+            $Kernel32 = Add-Type -MemberDefinition $Kernel32Definition -Name 'Kernel32' -Namespace 'Win32' -PassThru
+        }
+
+        $Shell32Definition = @'
+            [DllImport("shell32.dll", SetLastError = true)]
+            public static extern IntPtr CommandLineToArgvW(
+                [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine,
+                out int pNumArgs);
+'@
+        if (-Not $Shell32::CommandLineToArgvW) {
+            $Shell32 = Add-Type -MemberDefinition $Shell32Definition -Name 'Shell32' -Namespace 'Win32' -PassThru
+        }
+
+        if (!$CommandLine) {
+            $CommandLine = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Kernel32::GetCommandLineW())
+        }
+    }
+
+    Process {
+        $ParsedArgCount = 0
+        $ParsedArgsPtr = $Shell32::CommandLineToArgvW($CommandLine, [ref]$ParsedArgCount)
+
+        Try {
+            $ParsedArgs = @()
+
+            0..$ParsedArgCount | ForEach-Object {
+                $ParsedArgs += [System.Runtime.InteropServices.Marshal]::PtrToStringUni(
+                    [System.Runtime.InteropServices.Marshal]::ReadIntPtr($ParsedArgsPtr, $_ * [IntPtr]::Size)
+                )
+            }
+        }
+        Finally {
+            $Kernel32::LocalFree($ParsedArgsPtr) | Out-Null
+        }
+
+        $ret = @()
+
+        # -lt to skip the last item, which is a NULL ptr
+        for ($i = 0; $i -lt $ParsedArgCount; $i += 1) {
+            $ret += $ParsedArgs[$i]
+        }
+
+        # return $ret
+        return (, $ret)
+    }
+}
+
 function Invoke-Editor {
     if (Test-Path env:VISUAL) {
         $editor = $env:VISUAL
@@ -368,7 +477,10 @@ function Invoke-Editor {
         zed @args
     }
     else {
-        & $editor @args
+        $editorArgs = Split-CommandLine $editor
+        $editorCmd = $editorArgs[0]
+        $editorArgs = $editorArgs[1..($editorArgs.Length - 1)]
+        & $editorCmd @editorArgs @args
     }
 }
 New-Alias e Invoke-Editor -Force
