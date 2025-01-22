44dc564b2691c0e8098253611c62ca08a3421997
Christopher Larson <kergoth@gmail.com>
2025-01-21 19:09:49 -0700

    Move winget package installation into setup-system-windows directly
    This is to ease use of templates to control which packages are installed.

--- scripts/setup-system-windows.ps1
+++ scripts/setup-system-windows.ps1
@@ -90,7 +90,46 @@ if (-Not (Get-Command pwsh -ErrorAction SilentlyContinue)) {
     RefreshEnvPath
 }
 
-pwsh -NoProfile $PSScriptRoot\windows\install-wingetpackages.ps1
+# Install WinGet packages
+Install-ModuleIfNotInstalled Microsoft.WinGet.Client
+
+Import-Module Microsoft.WinGet.Client
+
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id AgileBits.1Password | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id AntibodySoftware.WizTree | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id 7zip.7zip | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Discord.Discord | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Ditto.Ditto | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id GnuPG.Gpg4win | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id AutoHotkey.AutoHotkey | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id BiniSoft.WindowsFirewallControl | Out-Null
+Install-winGetPackageIfNotInstalled -Mode Silent -Id egoist.devdocs-desktop | Out-Null
+Install-winGetPackageIfNotInstalled -Mode Silent -Id gerardog.gsudo | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Klocman.BulkCrapUninstaller | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.PowerShell | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.PowerToys | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.VisualStudio.2022.BuildTools | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.VisualStudioCode | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.WindowsTerminal | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Notepad++.Notepad++ | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id QL-Win.QuickLook | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id SyncTrayzor.SyncTrayzor | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Vivaldi.Vivaldi | Out-Null
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id VideoLAN.VLC | Out-Null
+
+# SnipDo
+Install-WinGetPackageIfNotInstalled -Mode Silent -Source msstore -Name SnipDo
+
+# WiFi Analyzer
+Install-WinGetPackageIfNotInstalled -Mode Silent -Source msstore -Id 9NBLGGH33N0N
+
+# Visual Studio C++ Desktop Workload
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.VisualStudio.2022.Community -Override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended"
+
+# GUI Apps for work
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Microsoft.Teams
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id PuTTY.PuTTY
+Install-WinGetPackageIfNotInstalled -Mode Silent -Id Rufus.Rufus
 
 # Configuration
 pwsh -NoProfile $PSScriptRoot\windows\configure-admin.ps1
