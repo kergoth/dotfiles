7bf103c38bec6d27f328a0b3036185eda79947b1
Christopher Larson <kergoth@gmail.com>
2025-01-21 19:09:48 -0700

    zsh: add support for atuin

--- home/dot_config/zsh/dot_zshrc.d/final.zsh
+++ home/dot_config/zsh/dot_zshrc.d/final.zsh
@@ -1,4 +1,12 @@
-if (( $+commands[mcfly] )); then
+if (( $+commands[atuin] )); then
+    atuin_cache="$XDG_CACHE_HOME/zsh/atuin.zsh"
+    if [[ ! -e $atuin_cache ]]; then
+        mkdir -p $XDG_CACHE_HOME/zsh
+        atuin init zsh >$atuin_cache || rm -f $atuin_cache
+    fi
+    source $atuin_cache
+    unset atuin_cache
+elif (( $+commands[mcfly] )); then
     MCFLY_PATH="$(command -v mcfly)"
 
     mcfly_cache="$XDG_CACHE_HOME/mcfly/env.zsh"
@@ -31,10 +39,6 @@ key[PageDown]=${terminfo[knp]}
 [[ -n "${key[End]}"      ]]  && bindkey  "${key[End]}"      end-of-line
 [[ -n "${key[Insert]}"   ]]  && bindkey  "${key[Insert]}"   overwrite-mode
 [[ -n "${key[Delete]}"   ]]  && bindkey  "${key[Delete]}"   delete-char
-#[[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       up-line-or-history
-[[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       history-substring-search-up
-#[[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     down-line-or-history
-[[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     history-substring-search-down
 [[ -n "${key[Left]}"     ]]  && bindkey  "${key[Left]}"     backward-char
 [[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"    forward-char
 [[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
@@ -43,18 +47,25 @@ key[PageDown]=${terminfo[knp]}
 bindkey "^[^[[D" backward-word
 bindkey "^[^[[C" forward-word
 
-# bind UP and DOWN arrow keys (compatibility fallback
-# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+if ! (( $+commands[atuin] )); then
+    #[[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       up-line-or-history
+    [[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       history-substring-search-up
+    #[[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     down-line-or-history
+    [[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     history-substring-search-down
 
-# bind P and N for EMACS mode
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
-
-# bind k and j for VI mode
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+    # bind UP and DOWN arrow keys (compatibility fallback
+    # for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+    bindkey '^[[A' history-substring-search-up
+    bindkey '^[[B' history-substring-search-down
+    
+    # bind P and N for EMACS mode
+    bindkey -M emacs '^P' history-substring-search-up
+    bindkey -M emacs '^N' history-substring-search-down
+    
+    # bind k and j for VI mode
+    bindkey -M vicmd 'k' history-substring-search-up
+    bindkey -M vicmd 'j' history-substring-search-down
+fi
 
 # Finally, make sure the terminal is in application mode, when zle is
 # active. Only then are the values from $terminfo valid.
