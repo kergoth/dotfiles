f7e39c2f2766e3389ab14efde51299a7cb2e80f7
Christopher Larson <chris.larson@siemens.com>
2025-01-23 21:53:43 -0700

    Also shift chezmoi init into bootstrap for ps1

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -21,3 +21,23 @@ if (-not (Get-Command chezmoi -ErrorAction SilentlyContinue)) {
   scoop install chezmoi
 }
 
+# Set path to this dotfiles repo
+$env:DOTFILES_DIR = $PSScriptRoot | Split-Path -Parent
+
+$chezmoidir = "$env:USERPROFILE\.local\share\chezmoi"
+if ($env:DOTFILES_DIR -ne $chezmoidir) {
+  if (-not (Test-Path "$env:USERPROFILE\.local\share")) {
+    New-Item -ItemType Directory -Path "$env:USERPROFILE\.local\share" | Out-Null
+  }
+  # Check for non-symbolic-link ~/.local/share/chezmoi
+  if ((Test-Path $chezmoidir) -and ((Get-Item $chezmoidir).LinkType -ne 'SymbolicLink')) {
+    throw "$chezmoidir exists and is not a symlink. Please remove it manually."
+  }
+  # Symlink to ~/.local/share/chezmoi to let chezmoi find the hook scripts
+  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.local\share\chezmoi" -Target $env:DOTFILES_DIR -Force | Out-Null
+}
+
+# Init my dotfiles
+Write-Output "Initializing dotfiles"
+chezmoi init
+Set-UserOnlyFileAccess $env:USERPROFILE\.config\chezmoi\chezmoi.toml
--- script/setup.ps1
+++ script/setup.ps1
@@ -7,24 +7,6 @@ if ($IsWindows) {
 
 & "$PSScriptRoot/bootstrap.ps1"
 
-# Set path to this dotfiles repo
-$env:DOTFILES_DIR = $PSScriptRoot | Split-Path -Parent
-
-$chezmoidir = "$env:USERPROFILE\.local\share\chezmoi"
-if ($env:DOTFILES_DIR -ne $chezmoidir) {
-  if (-not (Test-Path "$env:USERPROFILE\.local\share")) {
-    New-Item -ItemType Directory -Path "$env:USERPROFILE\.local\share" | Out-Null
-  }
-  # Check for non-symbolic-link ~/.local/share/chezmoi
-  if ((Test-Path $chezmoidir) -and ((Get-Item $chezmoidir).LinkType -ne 'SymbolicLink')) {
-    throw "$chezmoidir exists and is not a symlink. Please remove it manually."
-  }
-  # Symlink to ~/.local/share/chezmoi to let chezmoi find the hook scripts
-  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.local\share\chezmoi" -Target $env:DOTFILES_DIR -Force | Out-Null
-}
-
 # Apply my dotfiles
 Write-Output "Applying dotfiles"
-chezmoi init
 chezmoi apply --keep-going
-Set-UserOnlyFileAccess $env:USERPROFILE\.config\chezmoi\chezmoi.toml
