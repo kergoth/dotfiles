eba3e54386914dcf67c8cc4d9144fabcd0d0b6d9
Christopher Larson <kergoth@gmail.com>
2025-02-03 21:54:46 +0000

    zsh: remove the pkgx integration

--- home/dot_config/zsh/dot_zshrc.d/pkgx.zsh
+++
@@ -1,9 +0,0 @@
-if (( $+commands[pkgx] )); then
-    pkgx_cache="$XDG_CACHE_HOME/zsh/pkgx.zsh"
-    if [[ ! -e $pkgx_cache ]]; then
-        mkdir -p $XDG_CACHE_HOME/zsh
-        pkgx --shellcode >$pkgx_cache || rm -f $pkgx_cache
-    fi
-    source $pkgx_cache
-    unset pkgx_cache
-fi
