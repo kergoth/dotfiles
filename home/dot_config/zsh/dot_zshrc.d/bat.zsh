eadcb79f8b0673169b0408b349a2cedc782f5119
Christopher Larson <kergoth@gmail.com>
2025-02-11 16:12:04 +0000

    zsh: add bathelp/help to highlight usage/help output

--- home/dot_config/zsh/dot_zshrc.d/bat.zsh
+++ home/dot_config/zsh/dot_zshrc.d/bat.zsh
@@ -1,4 +1,11 @@
 if (( $+commands[bat] )); then
     export MANPAGER="sh -c 'col -bx | bat -l man -p'"
     export MANROFFOPT="-c"
+
+    bathelp() {
+        bat --plain --language=help "$@"
+    }
+    help() {
+        "$@" --help 2>&1 | bathelp
+    }
 fi
