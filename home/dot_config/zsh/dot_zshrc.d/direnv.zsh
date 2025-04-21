4741c9360998d3d7a532a937cebbb17dffa8627f
Christopher Larson <kergoth@gmail.com>
2025-04-21 12:29:50 -0700

    direnv: add nix shell zsh site functions to the fpath

--- home/dot_config/zsh/dot_zshrc.d/direnv.zsh
+++ home/dot_config/zsh/dot_zshrc.d/direnv.zsh
@@ -4,4 +4,28 @@ if (( ${+commands[direnv]} )); then
     else
         emulate zsh -c "$(direnv hook zsh)"
     fi
+
+    # Add zsh function paths from nix shell environment paths
+    function update_fpath_from_native_build_inputs() {
+      # Remove existing /nix/ paths from fpath
+      fpath=(${fpath:#/nix/*})
+
+      # Check if nativeBuildInputs is set (should be space-separated paths)
+      if [[ -n "$nativeBuildInputs" ]]; then
+        for buildpath in ${(z)nativeBuildInputs}; do
+          sitefuncs="$buildpath/share/zsh/site-functions"
+          if [[ -d "$sitefuncs" ]]; then
+            fpath+=("$sitefuncs")
+          fi
+        done
+      fi
+    }
+
+    if (( ! ${precmd_functions[(I)update_fpath_from_native_build_inputs]} )); then
+      precmd_functions=($precmd_functions update_fpath_from_native_build_inputs )
+    fi
+    typeset -ag chpwd_functions
+    if (( ! ${chpwd_functions[(I)update_fpath_from_native_build_inputs]} )); then
+      chpwd_functions=($chpwd_functions update_fpath_from_native_build_inputs)
+    fi
 fi
