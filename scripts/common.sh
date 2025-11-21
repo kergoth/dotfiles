b7854fa62d2c681d4150110dae3ffb718e40098d
Christopher Larson <chris.larson@pano.ai>
2025-11-21 15:53:19 -0700

    Improve brewfile_install handling

--- scripts/common.sh
+++ scripts/common.sh
@@ -28,14 +28,14 @@ run() {
 
 brewfile_install() {
     local brewfile="$1"
-    if [ -f "$brewfile" ]; then
+    if [ -f "$brewfile.tmpl.age" ] || echo "$brewfile" | grep -q "\.tmpl.age$"; then
+        cat "${brewfile%.tmpl.age}.tmpl.age" | chezmoi decrypt | chezmoi execute-template | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
+    elif [ -f "$brewfile.tmpl" ] || echo "$brewfile" | grep -q "\.tmpl$"; then
+        cat "${brewfile%.tmpl}.tmpl" | chezmoi execute-template | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
+    elif [ -f "$brewfile.age" ] || echo "$brewfile" | grep -q "\.age$"; then
+        cat "${brewfile%.age}.age" | chezmoi decrypt | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
+    elif [ -f "$brewfile" ]; then
         run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file="$brewfile"
-    elif [ -f "$brewfile.tmpl.age" ]; then
-        cat "$brewfile.tmpl.age" | chezmoi decrypt | chezmoi execute-template | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
-    elif [ -f "$brewfile.tmpl" ]; then
-        cat "$brewfile.tmpl" | chezmoi execute-template | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
-    elif [ -f "$brewfile.age" ]; then
-        cat "$brewfile.age" | chezmoi decrypt | run "$HOMEBREW_PREFIX/bin/brew" bundle --no-upgrade install --file=-
     else
         msg "No $brewfile found"
         return 1
