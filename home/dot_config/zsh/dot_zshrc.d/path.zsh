b3034691333079b7c5c1b0e0240e1a8a0bf6870d
Christopher Larson <kergoth@gmail.com>
2025-11-24 21:04:14 -0700

    Greatly simplify nix env setup in zsh

--- home/dot_config/zsh/dot_zshrc.d/path.zsh
+++ home/dot_config/zsh/dot_zshrc.d/path.zsh
@@ -62,33 +62,12 @@ else
     home_nix=
 fi
 
-for f in etc/profile.d/nix-daemon.sh etc/profile.d/nix.sh etc/profile.d/hm-session-vars.sh; do
-    for profile_dir in ~/.nix-profile ~/.nix/var/nix/profiles/per-user/$USER/profile \
-                       /nix/var/nix/profiles/per-user/$USER/profile \
-                       /nix/var/nix/profiles/default; do
-        if [ -e "$profile_dir/$f" ]; then
-            . "$profile_dir/$f"
-            break
-        fi
-    done
-done
-
-if [[ -e ~/.nix-profile ]]; then
-    manpath=(~/.nix-profile/share/man $manpath)
-    fpath=(~/.nix-profile/share/zsh/site-functions $fpath)
-    path=(~/.nix-profile/bin $path)
-    xdg_data_dirs=(~/.nix-profile/share $xdg_data_dirs)
+# Home Manager session vars
+if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+    source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 fi
 
-if [[ -n $buildInputs ]]; then
-    for p in $=buildInputs; do
-        manpath=($p/share/man $manpath)
-        fpath=($p/share/zsh/site-functions $fpath)
-        path=($p/bin $path)
-        xdg_data_dirs=($p/share $xdg_data_dirs)
-    done
-fi
-
+# De-duplication & Normalization
 path=( ${(u)^path:A}(N-/) )
 
 # add plugins & topic directories to fpath
