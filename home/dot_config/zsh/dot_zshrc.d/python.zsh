c63405568f96b71371cc1a9817286109de52227f
Christopher Larson <kergoth@gmail.com>
2025-11-24 20:52:36 -0700

    Drop obsolete pyenv/poetry shell integration

--- home/dot_config/zsh/dot_zshrc.d/python.zsh
+++ home/dot_config/zsh/dot_zshrc.d/python.zsh
@@ -10,11 +10,5 @@ export PYTHONWARNINGS=ignore:DEPRECATION
 
 alias ptpython='ptpython "--config-dir=$XDG_DATA_HOME/ptpython"'
 
-if (( $+commands[poetry] )) && [ ! -e "$XDG_CACHE_HOME/zsh/completions/_poetry" ]; then
-    mkdir -p $XDG_CACHE_HOME/zsh/completions
-    chmod 0700 $XDG_CACHE_HOME/zsh/completions
-    poetry completions zsh >"$XDG_CACHE_HOME/zsh/completions/_poetry"
-fi
-
 export PIPX_HOME=$XDG_DATA_HOME/../pipx
 export RUFF_CACHE_DIR="$XDG_CACHE_HOME/ruff"
