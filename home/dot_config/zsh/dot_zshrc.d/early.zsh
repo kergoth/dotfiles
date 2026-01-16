8cb800f086980737f44b252af71541d35852b632
Christopher Larson <kergoth@gmail.com>
2026-01-16 15:24:37 -0700

    Restore automatic dotfiles/vim updates

--- home/dot_config/zsh/dot_zshrc.d/early.zsh
+++ home/dot_config/zsh/dot_zshrc.d/early.zsh
@@ -1,3 +1,28 @@
+if [[ -z $DOTFILES_NO_UPDATE ]]; then
+    DOTFILES_STAMP=$XDG_CACHE_HOME/zsh/dotfiles-updated
+    old_stamps=( $DOTFILES_STAMP(N.mh+14) )
+    if [[ -e ~/.local/share/chezmoi/.git ]] && { (( ${#old_stamps} )) || [[ ! -f $DOTFILES_STAMP ]]; }; then
+        echo >&2 "Checking for dotfiles updates (once daily)"
+        chezmoi update && touch "$DOTFILES_STAMP" && echo >&2 "Restarting zsh for dotfiles updates" && \
+            if (( $+commands[direnv] )); then
+                exec direnv exec / zsh --login
+            else
+                exec zsh --login
+            fi
+    fi
+fi
+
+if [[ -e "$XDG_CONFIG_HOME/vim" ]] && [[ -z $VIM_NO_UPDATE ]]; then
+    VIM_STAMP=$XDG_CACHE_HOME/zsh/vim-updated
+    old_stamps=( $VIM_STAMP(N.mh+160) )
+    if [[ -e "$XDG_CONFIG_HOME/vim/.git" ]] && { (( ${#old_stamps} )) || [[ ! -f $VIM_STAMP ]]; }; then
+        echo >&2 "Checking for vim updates (once weekly)"
+        if [ -e "$XDG_CONFIG_HOME/vim/script/bootstrap" ]; then
+            "$XDG_CONFIG_HOME/vim/script/bootstrap" && touch "$VIM_STAMP"
+        fi
+    fi
+fi
+
 if [[ ! -e /.dockerenv ]]; then
     case "$(uname -r)" in
         *-microsoft-*)
