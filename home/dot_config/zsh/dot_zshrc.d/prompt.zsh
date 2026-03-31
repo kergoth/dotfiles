0c250556ae7337510ce1abb02241cfa5f9a7e7ac
Christopher Larson <kergoth@gmail.com>
2026-03-30 21:56:14 -0700

    Prototype starship+transient for zsh rather than powerlevel10k
    This aligns with my powershell prompt configuration.
    
    This prototype is part of investigation on #80.

--- home/dot_config/zsh/dot_zshrc.d/prompt.zsh
+++ home/dot_config/zsh/dot_zshrc.d/prompt.zsh
@@ -1,3 +1,23 @@
-if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
-    . $ZDOTDIR/themes/powerlevel10k/powerlevel10k.zsh-theme
+if [[ $TERM_PROGRAM == "WarpTerminal" ]]; then
+    PROMPT='%n@%m %1~ %# '
+    RPROMPT=
+    return
 fi
+
+if (( $+commands[starship] )); then
+    : ${STARSHIP_CONFIG:=${XDG_CONFIG_HOME:-$HOME/.config}/starship-right.toml}
+    export STARSHIP_CONFIG
+
+    if eval "$(starship init zsh)"; then
+        source "$ZDOTDIR/themes/transient-prompt/transient-prompt.zsh-theme"
+        # Starship renders the active prompt and zsh-transient-prompt rewrites accepted prompts.
+        TRANSIENT_PROMPT_PROMPT='$(starship prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+        TRANSIENT_PROMPT_RPROMPT='$(starship prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+        TRANSIENT_PROMPT_TRANSIENT_PROMPT='$(starship module character)'
+        TRANSIENT_PROMPT_TRANSIENT_RPROMPT=''
+        return
+    fi
+fi
+
+PROMPT='%n@%m %1~ %# '
+RPROMPT=
