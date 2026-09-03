281c9b26266e29745e7977f7504ea1ed456b35f6
Christopher Larson <chris.larson@pano.ai>
2026-09-03 09:47:33 -0700

    zsh: use vim for commit even if zed is installed

--- home/dot_config/zsh/dot_zshrc.d/zed.zsh
+++ home/dot_config/zsh/dot_zshrc.d/zed.zsh
@@ -1,4 +1,13 @@
 if [[ "$ZED_TERM" = true ]] && (( $+commands[zed] )); then
     export EDITOR="zed --wait"
     export VISUAL="$EDITOR"
+
+    # Zed does not hard wrap commit messages, use vim
+    if (( $+commands[nvim] )); then
+        export GIT_EDITOR=nvim
+    elif (( $+commands[vim] )); then
+        export GIT_EDITOR=vim
+    else
+        export GIT_EDITOR=vi
+    fi
 fi
