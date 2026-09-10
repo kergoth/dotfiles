c16b38ff2f4927ca0c77267848b1459d025324d7
Christopher Larson <kergoth@gmail.com>
2026-09-09 19:39:33 -0700

    zsh: simplify warp terminal handling

--- home/dot_config/zsh/dot_zshrc.d/prompt.zsh
+++ home/dot_config/zsh/dot_zshrc.d/prompt.zsh
@@ -1,6 +1,12 @@
+if [[ $TERM_PROGRAM == "WarpTerminal" ]]; then
+    PROMPT='%n@%m %1~ %# '
+    RPROMPT=
+    return
+fi
+
 if (( $+commands[starship] )); then
     export STARSHIP_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/starship-right.toml"
     eval "$(starship init zsh)"
-elif [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+else
     . "$ZDOTDIR/themes/powerlevel10k/powerlevel10k.zsh-theme"
 fi
